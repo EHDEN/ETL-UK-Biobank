fd946fec9098e53fd0f5f72ffe02f605c4a368ef
Alessia Peviani <alessia@thehyve.nl>
2020-09-17 17:07:12 +0200

    First go at main code

---
+++ main.py
@@ -0,0 +1,53 @@
+# Copyright 2020 The Hyve
+#
+# Licensed under the GNU General Public License, version 3,
+# or (at your option) any later version (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.gnu.org/licenses/
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# !/usr/bin/env python3
+
+import logging
+import sys
+import click
+from pathlib import Path
+from src.main.python.core.yaml import read_yaml_file
+from src.main.python.core.setup_logging import setup_logging
+from src.main.python.wrapper import Wrapper
+
+
+__version__ = '0.1.0'
+
+logger = logging.getLogger(__name__)
+
+@click.command()
+@click.option('--config', '-c', required=True, metavar='<config_file_path>',
+              help='Path to the yaml configuration file.',
+              type=click.Path(file_okay=True, exists=True, readable=True))
+def main(config):
+
+    # Load configuration
+    config = read_yaml_file(Path(config))
+
+    # Setup logging
+    debug: bool = config['run_options']['debug_mode']
+    setup_logging(debug)
+
+    # Initialize ETL with configuration parameters
+    etl = Wrapper(config)
+
+
+    logger.info('ETL version {}'.format(__version__))
+
+    # Run ETL
+    etl.run()
+
+if __name__ == "__main__":
+    sys.exit(main())
--- src/main/python/core/__init__.py
+++ src/main/python/core/__init__.py
@@ -2,4 +2,3 @@ from .EtlWrapper import EtlWrapper
 from .database import Database
 from .setup_logging import setup_logging
 from .source_data import SourceData
-from .yaml import read_yaml_file, write_yaml_file
