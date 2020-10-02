013b786da5479065801f64d07e947acec369b9a3
Alessia Peviani <alessia@thehyve.nl>
2020-10-02 14:38:13 +0200

    Add location_id mapping to baseline to Person script

--- src/main/python/transformation/baseline_to_person.py
+++ src/main/python/transformation/baseline_to_person.py
@@ -51,8 +51,8 @@ def baseline_to_person(wrapper: Wrapper) -> List[Wrapper.cdm.Person]:
             ethnicity_concept_id = 0,
             race_concept_id=race.get(row['21000-0.0'][0], 0),
             race_source_value=row['21000-0.0'],
-            # TODO: implement first location mapping, otherwise missing reference!
-            # location_id=row['54-0.0'] # alternatively, care_site_id
+            # TODO: use this or care_site?
+            location_id=row['54-0.0']
         )
         records.append(r)
 
