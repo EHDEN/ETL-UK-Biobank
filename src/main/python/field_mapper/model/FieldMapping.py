f1846a5fa65fd7617d96105a82b2a3b1b46c6b51
Maxim Moinat <maxim@thehyve.nl>
2020-10-26 19:26:27 +0100

    refactor FieldMapping and ValueMapping

--- src/main/python/field_mapper/model/FieldMapping.py
+++ src/main/python/field_mapper/model/FieldMapping.py
@@ -18,6 +18,7 @@ from src.main.python.field_mapper.model.UsagiModel import UsagiRow, TargetMappin
 
 
 class FieldMapping:
+    # TODO: refactor to make Field and Value Mapping use the same abstract class
 
     def __init__(self, field_id: str):
         self.field_id: str = field_id
@@ -25,11 +26,11 @@ class FieldMapping:
         # Initialize
         self.event_target: Optional[TargetMapping] = None
         self.unit_target: Optional[TargetMapping] = None
-        self.values: Dict[str, FieldMapping.ValueMapping] = {}
+        self.value_target: Optional[TargetMapping] = None  # Expected that this is not used much (only a field_id mapped to event AND value)
+        self.values: Dict[str, ValueMapping] = {}
         self.comment: Optional[str] = None
         self.status: Optional[MappingStatus] = None
         self.source_file_name: Optional[str] = None
-        self.is_ignored: bool = False
 
     def has_unit(self) -> bool:
         # TODO: what about numeric mappings that do not have a unit assigned?
@@ -60,46 +61,46 @@ class FieldMapping:
         else:
             self._add_field_mapping(usagi_row)
 
+    def set_status(self, status: MappingStatus):
+        # Although each Usagi row has a comment and status, these are given on field-value level
+        if self.status and self.status != status:
+            print(f'Warning: {self.field_id} has two conflicting statuses "{self.status}" and "{status}", the last is used')
+
+        if status == MappingStatus.IGNORED and (self.event_target or self.unit_target or self.value_target):
+            print(f'Warning: {self.field_id} is ignored and has target mappings. The mappings will not be used.')
+
+        self.status = status
+
+    def set_target(self, target: TargetMapping):
+        if target.type == MappingType.EVENT:
+            if self.event_target:
+                print(f'Warning: {self.field_id} already has a event_concept_id assigned and will be overwritten.')
+            self.event_target = target
+        elif target.type == MappingType.UNIT:
+            if self.unit_target:
+                print(f'Warning: {self.field_id} already has a unit_concept_id assigned and will be overwritten.')
+            self.unit_target = target
+        elif target.type == MappingType.VALUE:
+            if self.value_target:
+                print(f'Warning: {self.field_id} already has a value_concept_id assigned and will be overwritten.')
+            self.value_target = target
+        else:
+            print(f'Warning: unknown MappingType "{target.type}"')
+
     def _add_field_mapping(self, usagi_row: UsagiRow):
         """
         Mapping of just the the field_id to an event and (optionally) a unit.
         :param usagi_row:
         :return:
         """
-        # Although each Usagi row has a comment and status, these are given on field-value level
         self.comment = usagi_row.comment
-        self.status = usagi_row.status
 
+        self.set_status(usagi_row.status)
         if self.status == MappingStatus.IGNORED:
-            self.is_ignored = True
-            if self.event_target or self.unit_target:
-                print(f'Warning: {self.field_id} is ignored so the concept mapping will not be used.')
-            # There should not be any other mapping information in this row
-            return
-
-        if self.is_ignored:
             print(f'Warning: {self.field_id} is ignored so the concept mapping will not be used.')
-
-        if usagi_row.target.type == MappingType.EVENT:
-            if self.event_target:
-                print(f'Warning: {self.field_id} already has a event_concept_id assigned and will be overwritten.')
-                return
-            self.event_target = usagi_row.target
-        elif usagi_row.target.type == MappingType.UNIT:
-            if self.unit_target:
-                print(f'Warning: {self.field_id} already has a unit_concept_id assigned and will be overwritten.')
-                return
-            self.unit_target = usagi_row.target
-        elif usagi_row.target.type == MappingType.VALUE:
-            # TODO: why can't a field have a target event and a target value concept?
-            if usagi_row.value_code:
-                print(f'Warning: please use the method "self._add_value_mapping" for adding value mappings {usagi_row}')
-            else:
-                print(f'Warning: no value code given in the usagi input row for field usagi_row {usagi_row}')
-            print(f'Warning: mapping is not added {usagi_row}')
             return
-        else:
-            print(f'Warning: unknown MappingType "{usagi_row.target.type}"')
+
+        self.set_target(usagi_row.target)
 
     def _add_value_mapping(self, usagi_row: UsagiRow):
         """
@@ -108,72 +109,80 @@ class FieldMapping:
         :return:
         """
         # Create new if does not exist, otherwise retrieve mapping for value
-        value_mapping = self.values.setdefault(usagi_row.value_code, FieldMapping.ValueMapping(usagi_row.value_code))
+        value_mapping = self.values.setdefault(
+            usagi_row.value_code,
+            ValueMapping(usagi_row.value_code, self)
+        )
 
         # Although each Usagi row has a comment and status, these are given on field-value level in the tool.
         value_mapping.comment = usagi_row.comment
-        value_mapping.status = usagi_row.status
+        value_mapping.set_status(usagi_row.status)
 
         if value_mapping.status == MappingStatus.IGNORED:
-            value_mapping.is_ignored = True
-            if value_mapping.event_target or value_mapping.value_target:
-                print(f'Warning: {self.field_id}-{value_mapping.value_code} is ignored so the concept mapping will not be used.')
-            # There should not be any other mapping information in this row
-            return
-
-        if value_mapping.is_ignored:
             print(f'Warning: {self.field_id}-{value_mapping.value_code} is ignored so the concept mapping will not be used.')
-
-        if self.event_target:
-            print(f'Warning: {self.field_id} already has an event_concept_id assigned and we are trying to add a value. ?')
-        if self.unit_target:
-            print(f'Warning: {self.field_id} already has an unit_concept_id assigned and we are trying to add a value. ?')
-
-        if usagi_row.target.type == MappingType.EVENT:
-            if value_mapping.event_target:
-                print(f'Warning: {self.field_id}-{value_mapping.value_code} already has a event_concept_id assigned and will be overwritten.')
-                return
-            value_mapping.event_target = usagi_row.target
-        elif usagi_row.target.type == MappingType.UNIT:
-            print(f'Warning: if value given as code, we expect no unit {usagi_row}.')
             return
-        elif usagi_row.target.type == MappingType.VALUE:
-            if value_mapping.value_target:
-                print(f'Warning: {self.field_id}-{value_mapping.value_code} already has a value_concept_id assigned.')
-                return
-            value_mapping.value_target = usagi_row.target
-        else:
-            print(f'Warning: unknown MappingType "{usagi_row.target.type}"')
+
+        if self.event_target or self.unit_target or self.value_target:
+            # TODO: continuous fields with few categorical values (-1, -3)
+            print(f'Warning: {self.field_id} already has a mapping on field level and we are trying to add a mapping of the value.')
+
+        value_mapping.set_target(usagi_row.target)
 
     def __str__(self):
-        if self.is_ignored:
+        if self.status == MappingStatus.IGNORED:
             return f'{self.field_id} => IGNORED'
 
         values = ', '.join([str(v) for k, v in self.values.items()])
         return f'{self.field_id} => ' \
                f'event_target: [{self.event_target}], ' \
                f'unit_target: [{self.unit_target}], ' \
-               f'{len(self.values)}values: [{values}], ' \
+               f'value_target: [{self.value_target}], ' \
+               f'values ({len(self.values)}): [{values}], ' \
                f'comment: {self.comment}'
 
-    class ValueMapping:
 
-        def __init__(self, value_code: str):
-            self.value_code: str = value_code
-            self.comment: Optional[str] = None
-            self.status: Optional[MappingStatus] = None
+class ValueMapping:
 
-            # Initialize
-            self.is_ignored: bool = False
-            self.event_target: Optional[TargetMapping] = None
-            self.value_target: Optional[TargetMapping] = None
+    def __init__(self, value_code: str, parent_field: FieldMapping):
+        self.field: FieldMapping = parent_field
+        self.value_code: str = value_code
+        self.comment: Optional[str] = None
+        self.status: Optional[MappingStatus] = None
 
-        def __str__(self):
-            if self.is_ignored:
-                return f'{self.value_code} => IGNORED'
+        # Initialize
+        self.event_target: Optional[TargetMapping] = None
+        self.value_target: Optional[TargetMapping] = None
 
-            return f'{self.value_code} => [' \
-                   f'event_target: [{self.event_target}], ' \
-                   f'value_target: [{self.value_target}], ' \
-                   f'status: {self.status}, ' \
-                   f'comment: {self.comment}]'
+    def set_status(self, status: MappingStatus):
+        # Although each Usagi row has a comment and status, these are given on field-value level
+        if self.status and self.status != status:
+            print(f'Warning: {self.value_code} has two conflicting statuses "{self.status}" and "{status}", the last is used')
+
+        if status == MappingStatus.IGNORED and (self.event_target or self.value_target):
+            print(f'Warning: {self.value_code} is ignored and has target mappings. The mappings will not be used.')
+
+        self.status = status
+
+    def set_target(self, target: TargetMapping):
+        if target.type == MappingType.EVENT:
+            if self.event_target:
+                print(f'Warning: {self.field.field_id}-{self.value_code} already has a event_concept_id assigned and will be overwritten.')
+            self.event_target = target
+        elif target.type == MappingType.UNIT:
+            print(f'Warning: {self.field.field_id}-{self.value_code} if value given as code, we expect no unit.')
+        elif target.type == MappingType.VALUE:
+            if self.value_target:
+                print(f'Warning: {self.field.field_id}-{self.value_code} already has a value_concept_id assigned and will be overwritten.')
+            self.value_target = target
+        else:
+            print(f'Warning: unknown MappingType "{target.type}"')
+
+    def __str__(self):
+        if self.status == MappingStatus.IGNORED:
+            return f'{self.value_code} => IGNORED'
+
+        return f'{self.value_code} => [' \
+               f'event_target: [{self.event_target}], ' \
+               f'value_target: [{self.value_target}], ' \
+               f'status: {self.status}, ' \
+               f'comment: {self.comment}]'
