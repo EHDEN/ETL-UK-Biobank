c8b670f7a9dd34b588ab973d2300b2a33f73030c
Maxim Moinat <maxim@thehyve.nl>
2020-10-28 22:27:16 +0100

    apply refactoring to get_end_datetime

--- src/main/python/util/date_functions.py
+++ src/main/python/util/date_functions.py
@@ -26,7 +26,7 @@ def get_datetime(date: str = '', format='%Y-%m-%d', default_date='1970-01-01') -
      - 1st of January (if month missing)
     If the date is missing, returns given default date.
     :param date: string
-    :param sep: date separator, default "-"
+    :param sep: date format
     :param default_date: default start date ("1970-01-01")
     """
     if pd.isnull(date) or not date.strip():
@@ -46,24 +46,21 @@ def get_datetime(date: str = '', format='%Y-%m-%d', default_date='1970-01-01') -
         raise ValueError(f'unrecognized date value {date}')
 
 
-def get_end_datetime(date: str, sep='-', default_date='1970-01-01') -> datetime:
+def get_end_datetime(date: str, format='%Y-%m-%d', default_date='1970-01-01') -> datetime:
     """
-    Expects a date in the format **YYYY<sep>MM<sep>DD**,
-    where the default separator <sep> == "-".
-    If the date uses a different separator, you should specify it with the "sep" argument.
     If the date is incomplete, returns:
      - last of the month (if day missing)
      - 31st of December (if month missing)
     If the date is missing, returns a default date.
     :param date: string
-    :param sep: date separator, default "-"
+    :param format: date format
     :param default_date: default end date ("1970-01-01)
     """
-    end_date = get_datetime(date, sep, default_date)
+    end_date = get_datetime(date, format, default_date)
     if pd.notnull(date):
-        if len(date)==4: # only year
+        if len(date) == 4:  # only year
             end_date = end_date.replace(month=12, day=31)
-        elif len(date)==7: # year + month, assuming a single character separator
+        elif len(date) == 7:  # year + month, assuming a single character separator
             days_in_month = monthrange(end_date.year, end_date.month)[1]
             end_date = end_date.replace(day=days_in_month)
     return end_date
@@ -72,16 +69,20 @@ def get_end_datetime(date: str, sep='-', default_date='1970-01-01') -> datetime:
 if __name__ == '__main__':
     import numpy as np
     print(get_datetime('2020-02-18'))
-    print(get_datetime('2020/02/18', sep='/'))
+    print(get_datetime('2020/02/18', format='%Y/%m/%d'))
     print(get_datetime('2020-02'))
     print(get_datetime('2020'))
     print(get_datetime(''))
     print(get_datetime(None))
     print(get_datetime(np.nan))
     print(get_end_datetime('2020-02-18'))
-    print(get_end_datetime('2020/02/18', sep='/'))
+    print(get_end_datetime('2020/02/18', format='%Y/%m/%d'))
     print(get_end_datetime('2020-02'))
     print(get_end_datetime('2020'))
     print(get_end_datetime(''))
     print(get_end_datetime(None))
     print(get_end_datetime(np.nan))
+
+    print(get_datetime('5/12/2020', format='%d/%m/%Y'))
+    print(get_datetime('12/2020', format='%d/%m/%Y'))
+    print(get_datetime('2020', format='%d/%m/%Y'))
