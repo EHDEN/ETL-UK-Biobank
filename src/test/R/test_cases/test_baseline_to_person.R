08e901d18dac4370639aaa0a0a3736d6baa8db60
Maxim Moinat <maxim@thehyve.nl>
2020-11-02 17:00:30 +0100

    add observation period test cases

--- src/test/R/test_cases/test_baseline_to_person.R
+++ src/test/R/test_cases/test_baseline_to_person.R
@@ -7,8 +7,3 @@ expect_person(person_id = 201, gender_concept_id = 8532, race_concept_id = 38003
 declareTest(202, 'Test mixed race male')
 add_baseline(eid = '202', `31-0.0` = '1', `21000-0.0` = '2', `34-0.0` = '1961', `52-0.0` = '3')
 expect_person(person_id = 202, gender_concept_id = 8507, race_concept_id = 4212311, month_of_birth = 3, year_of_birth = 1961)
-
-add_baseline(eid = '301')
-add_baseline(eid = '302')
-add_baseline(eid = '303')
-add_baseline(eid = '304')
