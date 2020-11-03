66fcafc883370a3d2c4073f547123533a5314805
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2020-11-03 16:47:18 +0100

    covid to observation transformation

--- src/main/python/transformation/covid_to_observation.py
+++ src/main/python/transformation/covid_to_observation.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from ..util.date_functions import get_datetime
 
-from ..core.model import Observation, VisitOccurrence
+from ..core.model import Observation, VisitOccurrence, Person
 
 if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
@@ -15,31 +15,47 @@ if TYPE_CHECKING:
 
 def covid_to_observation(wrapper: Wrapper) -> List[Observation]:
     source = pd.DataFrame(wrapper.get_source_data('covid.csv'))
+    type_vocab = pd.read_csv('./resources/baseline_field_mapping/covid_spectype.csv')
 
     records = []
     with wrapper.db.session_scope() as session:
         for _, row in source.iterrows():
 
-            query = session.query(VisitOccurrence) \
-                .filter(VisitOccurrence.person_id == row['eid'] and VisitOccurrence.visit_start_date == row['specdate'])
+            date = get_datetime(row['specdate'], "%d/%m/%Y")
 
+            query_1 = session.query(Person) \
+                .filter(Person.person_id == row['eid'])
             try:
-                visit_record = query.one()
+                person_record = query_1.one()
+                person_id = person_record.person_id
+            except NoResultFound or MultipleResultsFound:
+                continue
+
+            query_2 = session.query(VisitOccurrence) \
+                .filter(VisitOccurrence.person_id == person_id) \
+                .filter(VisitOccurrence.visit_start_date == date)
+            try:
+                visit_record = query_2.one()
                 visit_id = visit_record.visit_occurrence_id
             except NoResultFound or MultipleResultsFound:
                 visit_id = None
 
-            date = get_datetime(row['specdate'], "%d/%m/%Y")
+            concept_id = type_vocab[type_vocab['sourceCode'] == int(row['spectype'])]['conceptId'].values[0]
+
+            result = {
+                '1' : 45884084, # Positive
+                '0' : 45878583 # Negative
+            }
 
             r = Observation(
-                person_id=row['eid'],
-                observation_concept_id=row['spectype'],
+                person_id=person_id,
+                observation_concept_id=int(concept_id),
                 observation_date=date.date(),
                 observation_datetime=date,
-                value_as_concept_id=row['result'],
-                observation_type_concept_id=38000279, # Lab observation concept code result
-                visit_occurrence_id =visit_id,
-                observation_source_value= row['spectype']
+                value_as_concept_id=result.get(row['result'], 0),
+                observation_type_concept_id=38000279,  # Lab observation concept code result
+                visit_occurrence_id=visit_id,
+                observation_source_value=row['spectype']
             )
             records.append(r)
         return records
---
+++ src/test/R/test_cases/test_covid_to_observation.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: sofiabazakou
+# Created on: 03/11/2020
+
