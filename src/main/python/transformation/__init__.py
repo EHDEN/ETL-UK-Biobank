07049becda16f461684dcc008388f4dcfa4203b8
Anne0507 <annevanwinzum@hotmail.com>
2020-11-04 10:03:47 +0100

    Fixes #52, Fixes #52

--- src/main/python/transformation/__init__.py
+++ src/main/python/transformation/__init__.py
@@ -1,6 +1,9 @@
 from .baseline_to_person import baseline_to_person
 from .assessment_center_to_location import assessment_center_to_location
 from .covid_to_care_site import covid_to_care_site
+from .gp_clinical_to_care_site import gp_clinical_to_care_site
 from .gp_clinical_to_visit_occurrence import gp_clinical_to_visit_occurrence
+from .covid_to_visit_occurrence import covid_to_visit_occurrence
+from .gp_prescriptions_to_visit_occurrence import gp_prescriptions_to_visit_occurrence
 from .gp_registrations_to_observation_period import gp_registrations_to_observation_period
 
--- src/main/python/transformation/gp_prescriptions_to_visit_occurrence.py
+++ src/main/python/transformation/gp_prescriptions_to_visit_occurrence.py
@@ -9,17 +9,17 @@ if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
 
-def gp_clinical_to_visit_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.VisitOccurrence]:
-    source = pd.DataFrame(wrapper.get_source_data('gp_clinical.csv'))
+def gp_prescriptions_to_visit_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.VisitOccurrence]:
+    source = pd.DataFrame(wrapper.get_source_data('gp_prescriptions.csv'))
 
     records = []
 
     for _, row in source.iterrows():
-        visit_date = get_datetime(row['event_dt'], "%d/%m/%Y")
+        visit_date = get_datetime(row['issue_date'], "%d/%m/%Y")
 
         r = wrapper.cdm.VisitOccurrence(
             person_id=row['eid'],
-            visit_concept_id=38004453,  # Family Practice
+            visit_concept_id=38004453,  # Family practice
             visit_start_date=visit_date.date(),
             visit_start_datetime=visit_date,
             visit_end_date=visit_date.date(),
