5c58c1908bd5dba931669924368490f61057f83e
Anne0507 <annevanwinzum@hotmail.com>
2020-11-10 16:40:33 +0100

    update tests for baseline to visit occurrence

--- src/test/R/test_cases/test_baseline_to_visit_occurrence.R
+++ src/test/R/test_cases/test_baseline_to_visit_occurrence.R
@@ -1,16 +1,16 @@
 # test IDs 700-799
 
 declareTest(700, 'baseline to visit occurrence')
-add_baseline(eid = 700, `53-0.0` = '2009-11-20', `53-1.0` = '2010-01-01', `53-2.0` = '2010-06-15', `53-3.0` = '2011-02-06', `54-0.0` = '10003')
+add_baseline(eid = 700, `53-0.0` = '2009-11-20', `53-1.0` = '2010-01-01', `53-2.0` = '2010-06-15', `53-3.0` = '2011-02-06')
 expect_count_visit_occurrence(4, person_id = 702, visit_concept_id = 44818519)
 expect_visit_occurrence(person_id = 700, visit_concept_id = 44818519, visit_start_date = '2009-11-20',
-                        visit_end_date = '2009-11-20', visit_type_concept_id = 44818519, care_site_id = 10003)
+                        visit_end_date = '2009-11-20', visit_type_concept_id = 44818519)
 expect_visit_occurrence(person_id = 700, visit_concept_id = 44818519, visit_start_date = '2010-01-01', 
-                        visit_end_date = '2010-01-01', visit_type_concept_id = 44818519, care_site_id = NULL)
+                        visit_end_date = '2010-01-01', visit_type_concept_id = 44818519)
 expect_visit_occurrence(person_id = 700, visit_concept_id = 44818519, visit_start_date = '2010-06-15', 
-                        visit_end_date = '2010-06-15', visit_type_concept_id = 44818519, care_site_id = NULL)
+                        visit_end_date = '2010-06-15', visit_type_concept_id = 44818519)
 expect_visit_occurrence(person_id = 700, visit_concept_id = 44818519, visit_start_date = '2011-02-06', 
-                        visit_end_date = '2011-02-06', visit_type_concept_id = 44818519, care_site_id = NULL)
+                        visit_end_date = '2011-02-06', visit_type_concept_id = 44818519)
 
 declareTest(701, 'baseline to visit occurrence with missing dates')
 add_baseline(eid = 701, `53-0.0` = '2009-11-20', `53-1.0` = '2010-01-01', `53-2.0` = '', `53-3.0` = '')
@@ -21,9 +21,9 @@ expect_visit_occurrence(person_id = 701, visit_concept_id = 44818519, visit_star
                         visit_end_date = '2010-01-01', visit_type_concept_id = 44818519)
 
 declareTest(702, 'baseline to visit occurrence with multiple centre of cares')
-add_baseline(eid = 702, `53-0.0` = '2009-11-20', `53-1.0` = '2010-01-01', `54-0.0` = '10003', `54-1.0` = '11013')
+add_baseline(eid = 702, `53-0.0` = '2009-11-20', `53-1.0` = '2010-01-01')
 expect_count_visit_occurrence(2, person_id = 702, visit_concept_id = 44818519)
 expect_visit_occurrence(person_id = 702, visit_concept_id = 44818519, visit_start_date = '2009-11-20',
-                        visit_end_date = '2009-11-20', visit_type_concept_id = 44818519, care_site_id = 10003)
+                        visit_end_date = '2009-11-20', visit_type_concept_id = 44818519)
 expect_visit_occurrence(person_id = 702, visit_concept_id = 44818519, visit_start_date = '2010-01-01',
-                        visit_end_date = '2010-01-01', visit_type_concept_id = 44818519, care_site_id = 11013)
+                        visit_end_date = '2010-01-01', visit_type_concept_id = 44818519)
