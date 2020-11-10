7cdf4ba477630bd6646b5ad5c971cef6862576e3
Anne0507 <annevanwinzum@hotmail.com>
2020-11-10 10:02:10 +0100

    Small changes to finalize

--- src/main/python/transformation/baseline_to_person.py
+++ src/main/python/transformation/baseline_to_person.py
@@ -31,12 +31,12 @@ def baseline_to_person(wrapper: Wrapper) -> List[Wrapper.cdm.Person]:
         r = wrapper.cdm.Person(
             person_id=row['eid'],
             person_source_value=row['eid'],
-            gender_concept_id=gender.get(row['31-0.0']),
+            gender_concept_id=gender.get(row['31-0.0'], 0),
             gender_source_value=row['31-0.0'],
             year_of_birth=int(row['34-0.0']),
             month_of_birth=int(row['52-0.0']),
             ethnicity_concept_id=0,
-            race_concept_id=race.get(row['21000-0.0']) or 0,
+            race_concept_id=race.get(row['21000-0.0'], 0),
             race_source_value=row['21000-0.0'],
             # TODO: use this or care_site?
             location_id=row['54-0.0']
