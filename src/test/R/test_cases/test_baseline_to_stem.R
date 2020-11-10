13d870c0106844e076b89a908d9aecfc84a8f59c
Maxim Moinat <maxim@thehyve.nl>
2020-11-10 22:02:50 +0100

    fix tests. skip year of birth as event

--- src/test/R/test_cases/test_baseline_to_stem.R
+++ src/test/R/test_cases/test_baseline_to_stem.R
@@ -1,6 +1,6 @@
-declareTest(900, 'Baseline - Hand grip to measurement')
+declareTest(900, 'Baseline - Hand grip')
 add_baseline(eid = '900', `53-0.0`='2010-10-10', `47-0.0`='11.3')
-expect_measurement(person_id = 900, measurement_date = '2010-10-10', measurement_concept_id = 44805438,
+expect_observation(person_id = 900, observation_date = '2010-10-10', observation_concept_id = 44805438,
                    unit_concept_id = 9529, value_as_number = 11.3)
 
 declareTest(901, 'Baseline - Alcohol use, daily to observation')
@@ -11,12 +11,12 @@ declareTest(902, 'Baseline - Alcohol use, not answered')
 add_baseline(eid = '902', `1558-0.0`='-3')
 expect_no_observation(person_id = 902, observation_concept_id = 4052351)
 
-declareTest(903, 'Baseline - White blood cell count')
+declareTest(903, 'Baseline - White blood cell count (alternate date field)')
 add_baseline(eid = '903', `30000-0.0`='5.55', `30002-0.0`='2020-11-01')
 expect_measurement(person_id = 903, measurement_date = '2020-11-01', measurement_concept_id = 3010813,
                    unit_concept_id = 4175206, value_as_number = 5.55)
 
-declareTest(904, 'Baseline - History of diabetes')
+declareTest(904, 'Baseline - Field 20002, Diabetes Mellitus')
 add_baseline(eid = '904', `53-0.0`='2020-11-02', `20002-0.0`='1220', `30002-0.0`='2020-11-01')
 expect_observation(person_id = 904, observation_date = '2020-11-02', observation_concept_id = 4188893, value_as_concept_id = 201820)
 
@@ -28,14 +28,14 @@ expect_observation(person_id = 905, observation_date = '2010-10-10', observation
 declareTest(906, 'Baseline - Chestpain present, repeat visit')
 add_baseline(eid = '906', `53-1.0`='2013-12-11', `2335-1.0`='1')
 expect_observation(person_id = 906, observation_date = '2013-12-11', observation_concept_id = 4188893,
-                   value_as_concept_id = 4133044, observation_source_value = '2335|0')
+                   value_as_concept_id = 4133044)
 
 declareTest(907, 'Baseline - Chestpain present, imaging visit')
-add_baseline(eid = '907', `53-1.0`='2014-12-11', `2335-2.0`='1')
+add_baseline(eid = '907', `53-2.0`='2014-12-11', `2335-2.0`='1')
 expect_observation(person_id = 907, observation_date = '2014-12-11', observation_concept_id = 4188893)
 
 declareTest(908, 'Baseline - Chestpain present, imaging repeat visit')
-add_baseline(eid = '908', `53-1.0`='2015-12-11', `2335-3.0`='1')
+add_baseline(eid = '908', `53-3.0`='2015-12-11', `2335-3.0`='1')
 expect_observation(person_id = 908, observation_date = '2015-12-11', observation_concept_id = 4188893)
 
 declareTest(909, 'Baseline - Chestpain ignored')
@@ -43,8 +43,8 @@ add_baseline(eid = '909', `53-0.0`='2013-09-05', `2335-0.0`='-1')
 expect_no_observation(person_id = 909, observation_date = '2013-9-5', observation_concept_id = 4188893)
 
 declareTest(910, 'Baseline - Do not map year of birth or gender')
-add_baseline(eid = '910', `34-0.0`='1970', `31-0.0`='0')
-expect_no_observation(person_id = 909, observation_source_value = '34')
+add_baseline(eid = '910', `31-0.0`='0', `34-0.0`='1970')
 expect_no_observation(person_id = 909, observation_source_value = '31|0')
+expect_no_observation(person_id = 909, observation_source_value = '34')
 
 # TODO: tests for array numbers
