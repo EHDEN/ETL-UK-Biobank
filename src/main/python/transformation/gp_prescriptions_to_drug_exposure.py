3af6675e510f5ecb17b20448d42605ae5c5395cd
Alessia Peviani <alessia@thehyve.nl>
2020-11-11 11:51:56 +0100

    Improve flow in transformation

--- src/main/python/transformation/gp_prescriptions_to_drug_exposure.py
+++ src/main/python/transformation/gp_prescriptions_to_drug_exposure.py
@@ -16,13 +16,13 @@ def gp_prescriptions_to_drug_exposure(wrapper: Wrapper) -> List[Wrapper.cdm.Drug
     source = pd.DataFrame(wrapper.get_source_data('gp_prescriptions.csv'))
 
     # map dm+d > read v2 > drug name
-    dmd_mapper = wrapper.code_mapper.generate_code_mapping_dictionary(
-        'dm+d',
-        restrict_to_codes=list(filter(None, set(source['dmd_code']))))
-    # Read v2 drug codes not available in Athena
-    # read2_mapper = wrapper.code_mapper.generate_code_mapping_dictionary(
-    #     'Read',
-    #     restrict_to_codes=list(filter(None, set(source['read_2']))))
+    dmd_codes = list(filter(None, set(source['dmd_code'])))
+    dmd_mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary('dm+d', restrict_to_codes=dmd_codes)
+    # Read v2 drug codes not available in Athena!
+    # read2_codes = list(filter(None, set(source['read_2'])))
+    # read2_mapper = \
+    #     wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read2_codes)
 
     records = []
     for _, row in source.iterrows():
@@ -39,13 +39,14 @@ def gp_prescriptions_to_drug_exposure(wrapper: Wrapper) -> List[Wrapper.cdm.Drug
                 drug_concept_id = mapping.target_concept_id
                 source_concept_id = mapping.source_concept_id
                 drug_col = 'dmd_code'
-        # TODO: implement mappings for other drug columns
+        # TODO: implement mappings for read2 column
         # if not drug_concept_id and row['read_2']:
         #     mapping = read2_mapper.lookup(row['read_2'], first_only=True, full_mapping=True)
         #     if mapping:
         #         drug_concept_id = mapping.target_concept_id
         #         source_concept_id = mapping.source_concept_id
         #         drug_col = 'read_2'
+        # TODO: implement mappings for drug_name column
         # if not drug_concept_id and row['drug_name']:
         #     mapping = # TODO: implement mapper
         #     if mapping:
