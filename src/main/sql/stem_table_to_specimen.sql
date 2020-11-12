23bef55d200903de157bc5db39cf37caecdd0ee2
Maxim Moinat <maxim@thehyve.nl>
2020-11-12 15:15:49 +0100

    map domain_id "Spec Anatomic Site" to Specimen table

--- src/main/sql/stem_table_to_specimen.sql
+++ src/main/sql/stem_table_to_specimen.sql
@@ -47,5 +47,5 @@ SELECT
 
 FROM @target_schema.stem_table
     LEFT JOIN vocab.concept USING (concept_id)
-WHERE concept.domain_id = 'Specimen'
+WHERE concept.domain_id = 'Specimen' OR concept.domain_id = 'Spec Anatomic Site'
 ;
