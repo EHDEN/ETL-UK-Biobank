06c7c1dcc01295a4a71ea0cc887ca12929b408be
Anne0507 <annevanwinzum@hotmail.com>
2020-11-12 15:55:07 +0100

    Rewrite HES to condition occurrence + implementing code mapper

--- src/main/python/transformation/hesin_diag_to_condition_occurrence.py
+++ src/main/python/transformation/hesin_diag_to_condition_occurrence.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import csv
 from typing import List, TYPE_CHECKING
 import pandas as pd
 
@@ -9,6 +10,7 @@ if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
 
+# TODO: Update mapping document
 def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.ConditionOccurrence]:
     hesin_diag = pd.DataFrame(wrapper.get_source_data('hesin_diag.csv'))
     hesin = pd.DataFrame(wrapper.get_source_data('hesin.csv'))
@@ -16,35 +18,80 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
     source = hesin_diag.merge(hesin, on=['eid', 'ins_index'], how='left', suffixes=('', '_x'))
     source = source.drop_duplicates(subset=['eid', 'admidate', 'diag_icd10'])
 
-    icd10 = wrapper.code_mapper.generate_code_mapping_dictionary('ICD10')
+    icd10 = wrapper.code_mapper.generate_code_mapping_dictionary('ICD10', remove_dot=True)
+    icd9 = wrapper.code_mapper.generate_code_mapping_dictionary('ICD9CM', remove_dot=True)
+
+    with open('./resources/mapping_tables/condition_type_concepts.csv') as f_in:
+        condition_type = csv.DictReader(f_in, delimiter=',')
+        condition_type_concept = {}
+
+        for row in condition_type:
+            condition_type_concept[row['sourceCode']] = row['conceptId']
 
     records = []
 
     for _, row in source.iterrows():
-        # TODO: Add some documentation
-        # TODO: Add ICD9 codes
-        if row['diag_icd10'] == '' or row['diag_icd10'] in ['W019', 'S7200']:
-            continue
-        elif row['diag_icd10'] in ['S5250', 'M4796', 'M2333', 'S2240', 'M8695']:
-            target_concept_id = icd10.lookup(insert_dot(row['diag_icd10'], '.', -2), first_only=True, full_mapping=True).target_concept_id
-        elif not icd10.lookup(row['diag_icd10']):
-            target_concept_id = icd10.lookup(insert_dot(row['diag_icd10'], '.', -1), first_only=True, full_mapping=True).target_concept_id
-        else:
-            target_concept_id = icd10.lookup(row['diag_icd10'], first_only=True, full_mapping=True).target_concept_id
+        condition_type_concept_id = condition_type_concept.get(row['level'])
 
         condition_date = get_datetime(row['admidate'], "%d/%m/%Y")
-        r = wrapper.cdm.ConditionOccurrence(
-            person_id=row['eid'],
-            condition_concept_id=target_concept_id,
-            condition_start_date=condition_date,
-            condition_start_datetime=condition_date,
-            condition_type_concept_id=0,  # TODO
-            condition_source_value=row['diag_icd10'],
-            condition_source_concept_id=0  # TODO
-        )
-        records.append(r)
+        # TODO: Add some documentation
+        if row['diag_icd10'] != '':
+            if not icd10.lookup(row['diag_icd10'], full_mapping=True):
+                target_concept_id = 0
+                condition_source_concept_id = 0
+                r = wrapper.cdm.ConditionOccurrence(
+                    person_id=row['eid'],
+                    condition_concept_id=target_concept_id,
+                    condition_start_date=condition_date,
+                    condition_start_datetime=condition_date,
+                    condition_type_concept_id=condition_type_concept_id,
+                    condition_source_value=row['diag_icd10'],
+                    condition_source_concept_id=condition_source_concept_id
+                )
+                records.append(r)
+            else:
+                for x in range(len(icd10.lookup(row['diag_icd10'], full_mapping=True))):
+                    target_concept_id = icd10.lookup(row['diag_icd10'], full_mapping=True)[x].target_concept_id
+                    condition_source_concept_id = icd10.lookup(row['diag_icd10'], full_mapping=True)[x].source_concept_id
+
+                    r = wrapper.cdm.ConditionOccurrence(
+                        person_id=row['eid'],
+                        condition_concept_id=target_concept_id,
+                        condition_start_date=condition_date,
+                        condition_start_datetime=condition_date,
+                        condition_type_concept_id=condition_type_concept_id,
+                        condition_source_value=row['diag_icd10'],
+                        condition_source_concept_id=condition_source_concept_id
+                    )
+                    records.append(r)
+
+        if row['diag_icd9'] != '':
+            if not icd9.lookup(row['diag_icd9'], full_mapping=True):
+                target_concept_id = 0
+                condition_source_concept_id = 0
+                r = wrapper.cdm.ConditionOccurrence(
+                    person_id=row['eid'],
+                    condition_concept_id=target_concept_id,
+                    condition_start_date=condition_date,
+                    condition_start_datetime=condition_date,
+                    condition_type_concept_id=condition_type_concept_id,
+                    condition_source_value=row['diag_icd9'],
+                    condition_source_concept_id=condition_source_concept_id
+                )
+                records.append(r)
+            else:
+                for x in range(len(icd9.lookup(row['diag_icd9'], full_mapping=True))):
+                    target_concept_id = icd9.lookup(row['diag_icd9'], full_mapping=True)[x].target_concept_id
+                    condition_source_concept_id = icd9.lookup(row['diag_icd9'], full_mapping=True)[x].source_concept_id
+
+                    r = wrapper.cdm.ConditionOccurrence(
+                        person_id=row['eid'],
+                        condition_concept_id=target_concept_id,
+                        condition_start_date=condition_date,
+                        condition_start_datetime=condition_date,
+                        condition_type_concept_id=condition_type_concept_id,
+                        condition_source_value=row['diag_icd9'],
+                        condition_source_concept_id=condition_source_concept_id
+                    )
+                    records.append(r)
     return records
-
-
-def insert_dot(string, dot, index):
-    return string[:index] + dot + string[index:]
