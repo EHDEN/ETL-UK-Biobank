eb35099ba75985413144eb6f0110b03c586d5047
Maxim Moinat <maxim@thehyve.nl>
2020-11-13 19:10:42 +0100

    add tests for hesin to visit

--- src/test/R/test_cases/test_hesin_to_visit_occurrence.R
+++ src/test/R/test_cases/test_hesin_to_visit_occurrence.R
@@ -1,6 +1,6 @@
-# test IDs 1100-1199
+# IDs 1100-1199
 
-declareTest(1100, 'Test emergency admission HES/2D')
+declareTest(1100, 'emergency admission HES/2D')
 add_baseline(eid = 1100)
 add_hesin(eid = 1100, dsource = 'HES',
           admidate = '13/01/1997', admimeth = '2D', admisorc = '',
@@ -9,7 +9,7 @@ expect_visit_occurrence(person_id = 1100, visit_start_date = '1997/01/13', visit
                         visit_concept_id = 9203, visit_source_value = 'record origin:HES/admission method:2D',
                         visit_type_concept_id = 44818517, discharge_to_concept_id = 0)
 
-declareTest(1101, 'Test admission source Hospice SMR/62')
+declareTest(1101, 'admission source Hospice SMR/62')
 add_baseline(eid = 1101)
 add_hesin(eid = 1101, dsource = 'SMR',
           admidate = '28/02/2001', admimeth = '', admisorc = '62',
@@ -17,7 +17,7 @@ add_hesin(eid = 1101, dsource = 'SMR',
 expect_visit_occurrence(person_id = 1101, visit_start_date = '2001/02/28', visit_end_date = '2001/11/01', visit_concept_id = 0, admitting_source_concept_id = 8546,
                         admitting_source_value = 'record origin:SMR/admission source:62')
 
-declareTest(1102, 'Test discharged to special hospital PEDW/49')
+declareTest(1102, 'discharged to special hospital PEDW/49')
 add_baseline(eid = 1102)
 add_hesin(eid = 1102, dsource = 'PEDW',
           admidate = '03/09/2007', admimeth = '', admisorc = '',
@@ -26,7 +26,7 @@ expect_visit_occurrence(person_id = 1102, visit_start_date = '2007/09/03', visit
                         visit_concept_id = 0, discharge_to_concept_id = 38004287,
                         discharge_to_source_value = 'record origin:PEDW/discharge destination:49')
 
-declareTest(1103, 'Test admission date missing')
+declareTest(1103, 'admission date missing')
 add_baseline(eid = 1103)
 add_hesin(eid = 1103, dsource = 'HES',
           admidate = '', admimeth = '11', admisorc = '54',
@@ -34,3 +34,20 @@ add_hesin(eid = 1103, dsource = 'HES',
 expect_visit_occurrence(person_id = 1103, visit_start_date = '1970/01/01', visit_end_date = '1991/12/06',
                         visit_concept_id = 9201, admitting_source_concept_id = 581476,
                         discharge_to_concept_id = 38004515)
+
+declareTest(1104, 'admission date and discharge date missing')
+add_baseline(eid = 1104)
+add_hesin(eid = 1104, admidate = '', disdate = '')
+expect_visit_occurrence(person_id = 1104, visit_start_date = '1970/01/01', visit_end_date = '1970/01/01')
+
+declareTest(1105, 'two episodes in one spell')
+add_baseline(eid = 1105)
+add_hesin(eid = 1105, spell_index = '0', admidate = '01/01/2010', disdate = '01/02/2010')
+add_hesin(eid = 1105, spell_index = '0', admidate = '01/02/2010', disdate = '01/03/2010')
+expect_visit_occurrence(person_id = 1105, visit_start_date = '2010/01/01', visit_end_date = '2010/03/01')
+
+declareTest(1106, 'two spells for one person results in two visits')
+add_baseline(eid = 1106)
+add_hesin(eid = 1106, spell_index = '0')
+add_hesin(eid = 1106, spell_index = '1')
+expect_count_visit_occurrence(2, person_id = 1106, visit_concept_id = 9201)
