df91c50b53dbafcdf4478be2687bb0e05a95b13f
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2020-11-13 13:23:06 +0100

    small fixes to transformation

--- src/main/python/transformation/hesin_to_visit_occurrence.py
+++ src/main/python/transformation/hesin_to_visit_occurrence.py
@@ -20,19 +20,19 @@ def hesin_to_visit_occurrence(wrapper: Wrapper) -> List[VisitOccurrence]:
     #  the latest discharge date
 
     visit_reason = pd.read_csv('./resources/mapping_tables/hesin_admimeth.csv')
-    visit_reason_lookup = visit_reason.groupby(['sourceCode', 'sourceValueName'])\
-                            .apply(lambda x: x['conceptId']\
-                            .values[0]).to_dict()
+    visit_reason_lookup = visit_reason.groupby(['sourceCode', 'ADD_INFO:coding_origin'])\
+        .apply(lambda x: x['conceptId'].values[0])\
+        .to_dict()
 
     admit_reason = pd.read_csv('./resources/mapping_tables/hesin_admisorc.csv')
-    admit_reason_lookup = admit_reason.groupby(['sourceCode', 'sourceValueName'])\
-                            .apply(lambda x: x['conceptId'].values[0])\
-                            .to_dict()
+    admit_reason_lookup = admit_reason.groupby(['sourceCode', 'ADD_INFO:coding_origin'])\
+        .apply(lambda x: x['conceptId'].values[0])\
+        .to_dict()
 
     dis_reason = pd.read_csv('./resources/mapping_tables/hesin_disdest.csv')
-    dis_reason_lookup = dis_reason.groupby(['sourceCode', 'sourceValueName'])\
-                            .apply(lambda x: x['conceptId'].values[0])\
-                            .to_dict()
+    dis_reason_lookup = dis_reason.groupby(['sourceCode', 'ADD_INFO:coding_origin'])\
+        .apply(lambda x: x['conceptId'].values[0])\
+        .to_dict()
 
     records = []
     with wrapper.db.session_scope() as session:
@@ -48,22 +48,25 @@ def hesin_to_visit_occurrence(wrapper: Wrapper) -> List[VisitOccurrence]:
                 continue
 
             start_date = get_datetime(row['admidate'], "%d/%m/%Y")
-
             end_date = get_end_datetime(row['disdate'], "%d/%m/%Y")
+
+            method_source = "record origin:"+row['dsource']+"/admission method:"+row['admimeth']
+            admit_source = "record origin:"+row['dsource']+"/admission source:"+row['admisorc']
+            dis_source = "record origin:"+row['dsource']+"/discharge destination:"+row['disdest']
 
             r = VisitOccurrence(
                 person_id=person_id,
-                visit_concept_id=visit_reason_lookup.get((row['dsource'], row['admimeth']), 0),
+                visit_concept_id=visit_reason_lookup.get((row['admimeth'], row['dsource']), 0),
                 visit_start_date=start_date.date(),
                 visit_start_datetime=start_date,
                 visit_end_date=end_date.date(),
                 visit_end_datetime=end_date,
                 visit_type_concept_id=44818517, # Visit derived from encounter on claim
-                visit_source_value=row['admimeth'],
-                admitting_source_concept_id=admit_reason_lookup.get((row['dsource'], row['admisorc']), 0),
-                admitting_source_value=row['admisorc'], # TODO add text to say what is the dsource
-                discharge_to_concept_id=dis_reason_lookup.get((row['dsource'], row['disdest']), 0),
-                discharge_to_source_value=row['disdest'] # TODO add text to say what is the dsource
+                visit_source_value=method_source[:50],
+                admitting_source_concept_id=admit_reason_lookup.get((row['admisorc'], row['dsource']), 0),
+                admitting_source_value=admit_source[:50],
+                discharge_to_concept_id=dis_reason_lookup.get((row['disdest'], row['dsource']), 0),
+                discharge_to_source_value=dis_source[:50]
             )
             records.append(r)
         return records
