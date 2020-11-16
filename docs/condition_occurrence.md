111d5a251f4b426e7fc844077a0ceb01a8dfbc62
Anne0507 <annevanwinzum@hotmail.com>
2020-11-16 12:13:56 +0100

    Add documentation and update mapping document

--- docs/condition_occurrence.md
+++ docs/condition_occurrence.md
@@ -11,14 +11,14 @@ Join hesin using ins_index
 | condition_occurrence_id |  |  | Auto-increment |
 | person_id | eid |  |  |
 | condition_concept_id | diag_icd10<br>diag_icd9 | Map to OMOP standard concept<br>Map to OMOP standard concept |  |
-| condition_start_date | ins_index | Join hesin on ins_index to get admidate. | hesin.admidate<br> |
-| condition_start_datetime | ins_index | Join hesin on ins_index to get admidate. | hesin.admidate<br> |
+| condition_start_date | ins_index | Join hesin on eid and ins_index to get admidate. | hesin.admidate<br> |
+| condition_start_datetime | ins_index | Join hesin on eid and ins_index to get admidate. | hesin.admidate<br> |
 | condition_end_date |  |  |  |
 | condition_end_datetime |  |  |  |
 | condition_type_concept_id | level | 1 - 44786627 primary condition  2 - 44786629 secondary condition  3 - 44786629 |  |
 | stop_reason |  |  |  |
 | provider_id |  |  |  |
-| visit_occurrence_id | ins_index | Lookup visit_occurrence_id by ins_index |  |
+| visit_occurrence_id | ins_index | Lookup visit_occurrence_id by spell_index |  |
 | visit_detail_id |  |  |  |
 | condition_source_value | diag_icd9<br>diag_icd10 |  |  |
 | condition_source_concept_id | diag_icd9<br>diag_icd10 |  |  |
--- src/main/python/transformation/hesin_diag_to_condition_occurrence.py
+++ src/main/python/transformation/hesin_diag_to_condition_occurrence.py
@@ -9,18 +9,23 @@ from ..util.date_functions import get_datetime
 if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
+# TODO: Add CareSite id
+# TODO: Add Visit Occurrence id
 
-# TODO: Update mapping document
+
 def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.ConditionOccurrence]:
     hesin_diag = pd.DataFrame(wrapper.get_source_data('hesin_diag.csv'))
     hesin = pd.DataFrame(wrapper.get_source_data('hesin.csv'))
 
+    # Merge HES diag with HES on EID and INS_INDEX to get ADMIDATE and drop duplicates.
     source = hesin_diag.merge(hesin, on=['eid', 'ins_index'], how='left', suffixes=('', '_x'))
-    source = source.drop_duplicates(subset=['eid', 'admidate', 'diag_icd10'])
+    source = source.drop_duplicates(subset=['eid', 'admidate', 'diag_icd10', 'diag_icd9'])
 
+    # Generate code mapping for ICD10 and ICD9, remove dot to get correct concept codes.
     icd10 = wrapper.code_mapper.generate_code_mapping_dictionary('ICD10', remove_dot=True)
     icd9 = wrapper.code_mapper.generate_code_mapping_dictionary('ICD9CM', remove_dot=True)
 
+    # Use Condition type concept file to map if primary condition or secondary condition.
     with open('./resources/mapping_tables/condition_type_concepts.csv') as f_in:
         condition_type = csv.DictReader(f_in, delimiter=',')
         condition_type_concept = {}
@@ -34,8 +39,9 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
         condition_type_concept_id = condition_type_concept.get(row['level'])
 
         condition_date = get_datetime(row['admidate'], "%d/%m/%Y")
-        # TODO: Add some documentation
+
         if row['diag_icd10'] != '':
+            # If ICD10 code is filled, but no concept is found, add concept ID 0.
             if not icd10.lookup(row['diag_icd10'], full_mapping=True):
                 target_concept_id = 0
                 condition_source_concept_id = 0
@@ -46,10 +52,13 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
                     condition_start_datetime=condition_date,
                     condition_type_concept_id=condition_type_concept_id,
                     condition_source_value=row['diag_icd10'],
-                    condition_source_concept_id=condition_source_concept_id
+                    condition_source_concept_id=condition_source_concept_id,
+                    # visit_occurrence_id=row['spell_index']
                 )
                 records.append(r)
             else:
+                # It is possible that multiple concepts are found for one ICD code,
+                # loop through the lookup to retrieve all of them and save concept ID and source concept ID.
                 for x in range(len(icd10.lookup(row['diag_icd10'], full_mapping=True))):
                     target_concept_id = icd10.lookup(row['diag_icd10'], full_mapping=True)[x].target_concept_id
                     condition_source_concept_id = icd10.lookup(row['diag_icd10'], full_mapping=True)[x].source_concept_id
@@ -61,11 +70,13 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
                         condition_start_datetime=condition_date,
                         condition_type_concept_id=condition_type_concept_id,
                         condition_source_value=row['diag_icd10'],
-                        condition_source_concept_id=condition_source_concept_id
+                        condition_source_concept_id=condition_source_concept_id,
+                        # visit_occurrence_id=row['spell_index']
                     )
                     records.append(r)
 
         if row['diag_icd9'] != '':
+            # If ICD9 code is filled, but no concept is found, add concept ID 0.
             if not icd9.lookup(row['diag_icd9'], full_mapping=True):
                 target_concept_id = 0
                 condition_source_concept_id = 0
@@ -76,10 +87,13 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
                     condition_start_datetime=condition_date,
                     condition_type_concept_id=condition_type_concept_id,
                     condition_source_value=row['diag_icd9'],
-                    condition_source_concept_id=condition_source_concept_id
+                    condition_source_concept_id=condition_source_concept_id,
+                    # visit_occurrence_id=row['spell_index']
                 )
                 records.append(r)
             else:
+                # It is possible that multiple concepts are found for one ICD code,
+                # loop through the lookup to retrieve all of them and save concept ID and source concept ID.
                 for x in range(len(icd9.lookup(row['diag_icd9'], full_mapping=True))):
                     target_concept_id = icd9.lookup(row['diag_icd9'], full_mapping=True)[x].target_concept_id
                     condition_source_concept_id = icd9.lookup(row['diag_icd9'], full_mapping=True)[x].source_concept_id
@@ -91,7 +105,8 @@ def hesin_diag_to_condition_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Con
                         condition_start_datetime=condition_date,
                         condition_type_concept_id=condition_type_concept_id,
                         condition_source_value=row['diag_icd9'],
-                        condition_source_concept_id=condition_source_concept_id
+                        condition_source_concept_id=condition_source_concept_id,
+                        # visit_occurrence_id=row['spell_index']
                     )
                     records.append(r)
     return records
