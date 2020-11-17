7a3a43d64109462ff1c0e0c02304e4eb361d6734
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2020-11-17 17:29:34 +0100

    hesin_oper transformation

--- src/main/python/transformation/hesin_oper_to_procedure_occurrence.py
+++ src/main/python/transformation/hesin_oper_to_procedure_occurrence.py
@@ -12,38 +12,40 @@ def hesin_oper_to_procedure_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Pro
     hesin_oper = pd.DataFrame(wrapper.get_source_data('hesin_oper.csv'))
     hesin = pd.DataFrame(wrapper.get_source_data('hesin.csv'))
 
-    source = hesin_oper.merge(hesin, on=['eid', 'ins_index'], how='left', suffixes=('', '_oper'))
+    source = hesin_oper.merge(hesin, on=['eid', 'ins_index'], how='left', suffixes=('', '_x'))
 
-    oper4_codes = wrapper.code_mapper.generate_code_mapping_dictionary('OPCS4')
+    oper4 = wrapper.code_mapper.generate_code_mapping_dictionary('OPCS4')
+    oper3 = wrapper.mapping_tables_lookup('./resources/mapping_tables/opcs3.csv')
+
+    procedure_type_concept = wrapper.mapping_tables_lookup('./resources/mapping_tables/procedure_type_concepts.csv')
+
     records = []
 
     for _, row in source.iterrows():
+        procedure_type_concept_id = procedure_type_concept.get(row['level'],0)
 
-        proc_date = get_datetime(row['opdate'], "%d/%m/%Y")
+        procedure_date = get_datetime(row['opdate'], "%d/%m/%Y")
 
-        if row['oper4'].notnull():
-            procedure = row['oper4']
-            procedure_concept = oper4_codes(row['oper4'])
-        elif row['oper3'].notnull():
-            procedure =  row['oper3']
-            procedure_concept = 0
+        if row['oper4'] != '':
+            procedure_targets = oper4.lookup(row['oper4'])
+        elif row['oper3'] != '':
+            procedure_targets = oper3.get(row['oper3'])
         else:
             continue
 
+        visit_occurrence_id = wrapper.lookup_visit(row['eid'], 'HES-' + str(row['spell_index']))
 
-        level = {
-            1: 44786630, # Primary Procedure
-            2: 44786631 # Secondary Procedure
-        }
-
-        r = wrapper.cdm.ProcedureOccurrence(
-            person_id=row['eid'],
-            procedure_concept_id= procedure_concept,
-            procedure_date=proc_date,
-            procedure_datetime=proc_date,
-            procedure_type_concept_id=level,
-            procedure_source_value=procedure,
-            procedure_source_concept_id=procedure
-        )
-        records.append(r)
+        for target in procedure_targets:
+            r = wrapper.cdm.ProcedureOccurrence(
+                person_id=row['eid'],
+                procedure_concept_id=target.target_concept_id,
+                procedure_date=procedure_date,
+                procedure_datetime=procedure_date,
+                procedure_type_concept_id=procedure_type_concept_id,
+                procedure_source_value=row['oper4'],
+                procedure_source_concept_id=target.source_concept_id,
+                visit_occurrence_id=visit_occurrence_id,
+                data_source=f'HES-{row["dsource"]}'
+            )
+            records.append(r)
     return records
