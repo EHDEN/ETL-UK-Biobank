4c022f6334d6ec86830dcc153053f6e09702fa9c
Alessia Peviani <alessia@thehyve.nl>
2020-11-18 14:23:52 +0100

    Add unit mapping in transformation

--- src/main/python/transformation/gp_clinical_to_stem_table.py
+++ src/main/python/transformation/gp_clinical_to_stem_table.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import List, TYPE_CHECKING
 import pandas as pd
+import csv
 from sqlalchemy.orm.exc import NoResultFound
 
 from ..util.date_functions import get_datetime
@@ -15,14 +16,27 @@ def gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm.StemTable]:
 
     source = pd.DataFrame(wrapper.get_source_data('gp_clinical.csv'))
 
-    read_codes = list(filter(None, set(source['read_2']) | set(source['read_3'])))
+    GP_CLINICAL_MAPPING_FOLDER = 'resources/gp_clinical_field_mapping/'
+
+    # load dictionary of valid units
+    with open(GP_CLINICAL_MAPPING_FOLDER + 'unit_raw_to_clean.csv') as f:
+        next(f)  # Skip provenance info
+        next(f)  # Skip the header
+        reader = csv.reader(f)
+        valid_units = dict(reader)
+
+    read2_codes = list(filter(None, set(source['read_2'])))
+    read3_codes = list(filter(None, set(source['read_3'])))
     # Read codes in the source are either all alphanumeric, or containing trailing dots;
     # however in OMOP Read vocabulary, dots are always followed by cyphers.
     # To find a mapping for the code, you need to add cyphers after the dots (by default, "00")
-    read_codes = [code + '00' if code[-1] == '.' else code for code in read_codes]
+    read2_codes = [code + '00' if code[-1] == '.' else code for code in read2_codes]
+    read3_codes = [code + '00' if code[-1] == '.' else code for code in read3_codes]
 
-    read_mapper = \
-        wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read_codes)
+    read2_mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read2_codes)
+    read3_mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read3_codes)
 
     # x = read_mapper.lookup('4I16.00', full_mapping=True)
     # for match in x:
@@ -35,7 +49,8 @@ def gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm.StemTable]:
 
     for _, row in source.iterrows():
 
-        # read_2 and read_3 should be mutually exclusive.. should
+        # read_2 and read_3 should be mutually exclusive
+        # and at least one should be present for the mapping to be meaningful
         if row['read_2']:
             read_code = row['read_2']
             read_col = 'read_2'
@@ -70,20 +85,18 @@ def gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm.StemTable]:
         if row['value3']:
             if row['value3'].startswith('OPR'):
                 operator = row['value3'][3:]
-            # TODO: how to handle MEAxxx codes?
-            # elif ['value3'].startswith('MEA'):
-                # do something
-            else:
+            elif unit in valid_units: # this includes MEAxxx codes
                 unit = row['value3']
-                unit_concept_id = 12345 # TODO: placeholder, create mapping table for units
+                if valid_units[unit]: # might still map to empty string
+                    unit_concept_id=12345 # TODO: placeholder, add mapping to standard concept_id
+                    unit_concept_id=0
 
         for value_col in ['value1', 'value2']:
             # for most rows only one of the two value fields will be provided,
             # for some though you need to process both, therefore this loop.
             # if value1 is empty, skip to value2;
             # if value2 is also empty, skip record creation
-            # TODO: only map codes that have logic captured in phenotype repo for now,
-            #   decide on how to handle the rest (especially rows missing both values)
+            # TODO: always ok to skip rows missing both values, or are there exceptions?
             value = row[value_col]
             if pd.isnull(value):
                 continue
@@ -92,11 +105,11 @@ def gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm.StemTable]:
                 value_as_concept_id = None
             except Exception:
                 value_as_number = None
-                value_as_concept_id = 0
+                value_as_concept_id = 0 # TODO: placeholder, create mapping table for alphanum codes (or always ignore?)
 
             r = wrapper.cdm.StemTable(
                 person_id=person_id,
-                type_concept_id=32020,  # TODO: different options depending if meas/diag/obs, see docs
+                type_concept_id=32020,  # TODO: add different options depending if meas/diag/obs
                 start_date=event_date,
                 start_datetime=event_date,
                 visit_occurrence_id=visit_id,
