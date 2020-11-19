b085759150fa72e4e7c675d33bff9aeb71bfda61
Alessia Peviani <alessia@thehyve.nl>
2020-11-19 02:22:20 +0100

    Add logic for drug processing

--- src/main/python/transformation/gp_prescriptions_to_drug_exposure.py
+++ src/main/python/transformation/gp_prescriptions_to_drug_exposure.py
@@ -4,8 +4,9 @@ from typing import List, TYPE_CHECKING
 import pandas as pd
 from datetime import timedelta
 from sqlalchemy.orm.exc import NoResultFound
-from src.main.python.util import get_datetime, extend_read_code
-from ..core.model import VisitOccurrence
+from src.main.python.util import get_datetime, filter_nulls, extend_read_code
+from src.main.python.core.model import VisitOccurrence
+from src.main.python.core.code_mapper import CodeMapping
 
 
 if TYPE_CHECKING:
@@ -15,40 +16,32 @@ def gp_prescriptions_to_drug_exposure(wrapper: Wrapper) -> List[Wrapper.cdm.Drug
 
     source = pd.DataFrame(wrapper.get_source_data('gp_prescriptions.csv'))
 
-    # map dm+d > read v2 > drug name
     dmd_mapper = \
         wrapper.code_mapper.generate_code_mapping_dictionary('dm+d', restrict_to_codes=list(source['dmd_code']))
     # TODO: Read v2 codes not available in Athena & NHS, find other mapping source if possible
-    # read2_codes = list(extend_read_code(code) for code in source['read_2'])
-    # read2_mapper = \
-    #     wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read2_codes)
+    read2_codes = list(extend_read_code(code) for code in filter(filter_nulls,source['read_2']))
+    read2_mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary('Read', restrict_to_codes=read2_codes)
 
     records = []
     for _, row in source.iterrows():
 
-        # drug mappings
-        drug_concept_id, source_concept_id = '', ''
-        if row['dmd_code']:
+        # TODO: in theory read v2 > drug name, implement Read v2 mapping and invert check order
+        drug_col = None
+        mapping = CodeMapping()
+        if filter_nulls(row['dmd_code']):
+            drug_col = 'dmd_code'
             mapping = dmd_mapper.lookup(row['dmd_code'], first_only=True)
-            if mapping:
-                drug_concept_id = mapping.target_concept_id
-                source_concept_id = mapping.source_concept_id
-                drug_col = 'dmd_code'
-        # TODO: implement mappings for read2 column
-        # if not drug_concept_id and row['read_2']:
-        #     mapping = read2_mapper.lookup(row['read_2'], first_only=True, full_mapping=True)
-        #     if mapping:
-        #         drug_concept_id = mapping.target_concept_id
-        #         source_concept_id = mapping.source_concept_id
-        #         drug_col = 'read_2'
-        # TODO: implement mappings for drug_name column
-        # if not drug_concept_id and row['drug_name']:
-        #     mapping = # TODO: implement mapper
-        #     if mapping:
-        #         drug_concept_id = # TODO: get concept_id from mapping
-        #         source_concept_id = None
-        #         drug_col = 'drug_name'
-        if not drug_concept_id:
+        if filter_nulls(row['drug_name']) and not mapping.target_concept_id:
+            drug_col = 'drug_name'
+            mapping = CodeMapping()
+            mapping.source_concept_code = row['drug_name']
+            mapping.source_concept_id = 0
+            mapping.target_concept_id = 0 # TODO: placeholder, get from mapping tables
+        if filter_nulls(row['read_2']) and not mapping.target_concept_id:
+            drug_col = 'read_2'
+            mapping = read2_mapper.lookup(row['read_2'], first_only=True)
+        if not drug_col:
             continue
 
         date_start = get_datetime(row['issue_date'], format='%d/%m/%Y')
@@ -82,9 +75,9 @@ def gp_prescriptions_to_drug_exposure(wrapper: Wrapper) -> List[Wrapper.cdm.Drug
             drug_exposure_start_datetime=date_start,
             drug_exposure_end_date=date_end,
             drug_exposure_end_datetime=date_end,
-            drug_concept_id=drug_concept_id,
-            drug_source_concept_id=source_concept_id,
-            drug_source_value=row[drug_col],
+            drug_concept_id=mapping.target_concept_id,
+            drug_source_concept_id=mapping.source_concept_id,
+            drug_source_value=mapping.source_concept_code,
             drug_type_concept_id=38000177,  # prescription written
             quantity=None,  # TODO: placeholder
             dose_unit_source_value=unit,
