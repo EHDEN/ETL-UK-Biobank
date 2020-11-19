4c4f5d25a8c2fd629f2d5222befc5a9bad8131c3
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2020-11-19 20:36:16 +0100

    transformation without the oper3 mapping

--- src/main/python/transformation/hesin_oper_to_procedure_occurrence.py
+++ src/main/python/transformation/hesin_oper_to_procedure_occurrence.py
@@ -3,19 +3,23 @@ import csv
 from typing import List, TYPE_CHECKING
 import pandas as pd
 from ..util.date_functions import get_datetime
+# from ..core.code_mapper import CodeMapping
 
 if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
 
 def hesin_oper_to_procedure_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.ProcedureOccurrence]:
-    hesin_oper = pd.DataFrame(wrapper.get_source_data('hesin_oper.csv'))
-    hesin = pd.DataFrame(wrapper.get_source_data('hesin.csv'))
+    hesin_oper = wrapper.get_dataframe('hesin_oper.csv')
+    hesin = wrapper.get_dataframe('hesin.csv')
+    hesin = hesin.drop_duplicates(subset=['eid', 'ins_index'])  # fix for synthetic data
 
     source = hesin_oper.merge(hesin, on=['eid', 'ins_index'], how='left', suffixes=('', '_x'))
 
-    oper4 = wrapper.code_mapper.generate_code_mapping_dictionary('OPCS4')
-    oper3 = wrapper.mapping_tables_lookup('./resources/mapping_tables/opcs3.csv')
+    oper4 = wrapper.code_mapper.generate_code_mapping_dictionary('OPCS4', remove_dot_from_codes=True)
+
+    # oper3 = wrapper.mapping_tables_lookup('./resources/mapping_tables/opcs3.csv', first_only=False)
+    # To be used when map the oper3 codes see below
 
     procedure_type_concept = wrapper.mapping_tables_lookup('./resources/mapping_tables/procedure_type_concepts.csv')
 
@@ -26,13 +30,21 @@ def hesin_oper_to_procedure_occurrence(wrapper: Wrapper) -> List[Wrapper.cdm.Pro
 
         procedure_date = get_datetime(row['opdate'], "%d/%m/%Y")
 
-        if row['oper4'] != '':
+        if not pd.isnull(row['oper4']):
             procedure_targets = oper4.lookup(row['oper4'])
-        elif row['oper3'] != '':
-            procedure_targets = oper3.get(row['oper3'])
+
+        # TODO: If there is no value in oper4 should look in oper3, currently the code below fails the tests.
+        # elif not pd.isnull(row['oper3']):
+        #     procedure_targets = []
+        #     for value in oper3.get(row['oper3']):
+        #         procedure_target = CodeMapping()
+        #         procedure_target.source_concept_code = row['oper3']
+        #         procedure_target.target_concept_id = value
+        #         procedure_targets.append(procedure_target)
         else:
             continue
 
+        # Visit
         visit_occurrence_id = wrapper.lookup_visit(row['eid'], 'HES-' + str(row['spell_index']))
 
         for target in procedure_targets:
--- src/test/R/test_cases/test_hes_oper_to_procedure_occurrence.R
+++ src/test/R/test_cases/test_hes_oper_to_procedure_occurrence.R
@@ -1,62 +1,65 @@
 # test IDs 1500-1599
 
-# Test ICD10 code
-declareTest(1500, 'Test OPCS4 code')
+# Test OPCS4 code - standard
+declareTest(1500, 'Test OPCS4 code - standard')
 add_baseline(eid = 1500)
-add_hesin(eid = 1500, ins_index = 0, admidate = '01/04/2016')
-add_hesin_oper(eid = 1500, ins_index = 0, oper4 = 'X998', level = 1)
-expect_procedure_occurrence(person_id = 1500, condition_concept_id = 44789899, procedure_date = '2016-04-01',
-                            procedure_type_concept_id = 44786630, condition_source_concept_id = 46233445)
+add_hesin(eid = 1500, ins_index = 0)
+add_hesin_oper(eid = 1500, ins_index = 0, opdate = '01/04/2016', oper4 = 'Z942', level = 1)
+expect_procedure_occurrence(person_id = 1500, procedure_concept_id = 44517227, procedure_date = '2016-04-01',
+                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 44517227)
 
 
-# Test ICD9 code
-declareTest(1501, 'Test OPCS3 code')
+# Test OPCS4 code - non standard
+declareTest(1501, 'Test OPCS4 code - non standard')
 add_baseline(eid = 1501)
-add_hesin(eid = 1501, ins_index = 1, admidate = '19/04/2018')
-add_hesin_oper(eid = 1501, ins_index = 1, oper3 = '704', oper4 = '', level = 1)
-expect_procedure_occurrence(person_id = 1501, condition_concept_id = 4074865, procedure_date = '2018-04-19',
-                            procedure_type_concept_id = 44786630)
+add_hesin(eid = 1501, ins_index = 0)
+add_hesin_oper(eid = 1501, ins_index = 0, opdate = '10/09/2018', oper4 = 'G451', level = 1)
+expect_procedure_occurrence(person_id = 1501, procedure_concept_id = 4071407, procedure_date = '2018-09-10',
+                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 44509820)
 
 
-# Test ICD9 and ICD10 code
-declareTest(1502, 'Test OPCS3 and OPCS4 code; map OPCS4')
-add_baseline(eid = 1502)
-add_hesin(eid = 1502, ins_index = 2, admidate = '30/12/2019')
-add_hesin_oper(eid = 1502, ins_index = 2, oper3 = '687', oper4 = 'Z942', level = 2)
-expect_procedure_occurrence(person_id = 1502, procedure_concept_id = 44517227, procedure_date = '2019-12-30',
-                            procedure_type_concept_id = 44786631, procedure_source_concept_id = 44517227)
+# Test OPCS3 code
+#declareTest(1502, 'Test OPCS3 code')
+#add_baseline(eid = 1502)
+#add_hesin(eid = 1502, ins_index = 1)
+#add_hesin_oper(eid = 1502, ins_index = 1, opdate = '19/04/2018', oper3 = '704', oper4 = '', level = 1)
+#expect_procedure_occurrence(person_id = 1501, procedure_concept_id = 4074865, procedure_date = '2018-04-19',
+#                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 0)
+
+
+# Test OPCS3 and OPCS4 code
+#declareTest(1503, 'Test OPCS3 and OPCS4 code; map OPCS4')
+#add_baseline(eid = 1503)
+#add_hesin(eid = 1503, ins_index = 2)
+#add_hesin_oper(eid = 1503, ins_index = 2, opdate = '30/12/2019', oper3 = '687', oper4 = 'X403', level = 2)
+#expect_procedure_occurrence(person_id = 1503, procedure_concept_id = 4120120, procedure_date = '2019-12-30',
+#                            procedure_type_concept_id = 44786631, procedure_source_concept_id = 44515557)
+
 
 # Test No concept found
-declareTest(1503, 'No concept found')
-add_baseline(eid = 1503)
-add_hesin(eid = 1503, ins_index = 3, admidate = '01/05/2014')
-add_hesin_oper(eid = 1503, ins_index = 3, oper4 = '', level = 2)
-expect_procedure_occurrence(person_id = 1503, procedure_concept_id = 0, procedure_date = '2014-05-01',
-                            procedure_type_concept_id = 44786631, procedure_source_concept_id = 0)
-
-# Test Source id but no target ID
-declareTest(1504, 'Test Source id but no target ID')
+declareTest(1504, 'No concept found')
 add_baseline(eid = 1504)
-add_hesin(eid = 1504, ins_index = 4, admidate = '09/03/2012')
-add_hesin_oper(eid = 1504, ins_index = 4, oper4 = '', level = 1)
-expect_procedure_occurrence(person_id = 1504, procedure_concept_id = 0, procedure_start_date = '2012-03-09',
-                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 0)
+add_hesin(eid = 1504, ins_index = 3)
+add_hesin_oper(eid = 1504, ins_index = 3, opdate = '01/05/2014', oper4 = 'X998', level = 2)
+expect_procedure_occurrence(person_id = 1504, procedure_concept_id = 0, procedure_date = '2014-05-01',
+                            procedure_type_concept_id = 44786631, procedure_source_value = 'X998')
 
 
 # Test if code has more > 1 code
-declareTest(1505, 'Test if code has more > 1 target concept')
-add_baseline(eid = 1505)
-add_hesin(eid = 1505, ins_index = 5, admidate = '25/08/2017')
-add_hesin_oper(eid = 1505, ins_index = 5, oper4 = '', level = 1)
-expect_procedure_occurrence(person_id = 1505, procedure_concept_id = , procedure_date = '2017-08-25',
-                            procedure_type_concept_id = 44786630, procedure_source_concept_id = )
-expect_procedure_occurrence(person_id = 1505, procedure_concept_id = 434547, procedure_date = '2017-08-25',
-                            procedure_type_concept_id = 44786630, procedure_source_concept_id = )
+#declareTest(1505, 'Test if code has more > 1 target concept')
+#add_baseline(eid = 1505)
+#add_hesin(eid = 1505, ins_index = 5)
+#add_hesin_oper(eid = 1505, ins_index = 5, opdate = '25/08/2017', oper3 = '4011', oper4 = '', level = 1)
+#expect_procedure_occurrence(person_id = 1505, procedure_concept_id = 4073667, procedure_date = '2017-08-25',
+#                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 0)
+#expect_procedure_occurrence(person_id = 1505, procedure_concept_id = 2109458, procedure_date = '2017-08-25',
+#                            procedure_type_concept_id = 44786630, procedure_source_concept_id = 0)
+
 
 declareTest(1506, 'HES diagnosis with visit')
 add_baseline(eid = 1506)
-add_hesin(eid = 1506, ins_index = 0, admidate = '18/12/2020')
-add_hesin_oper(eid = 1506, ins_index = 0, oper4 = '')
-expect_visit_occurrence(person_id = 1506, visit_start_date = '2020-12-18')
+add_hesin(eid = 1506, ins_index = 0, admidate = '11/12/2020')
+add_hesin_oper(eid = 1506, ins_index = 0, opdate = '18/12/2020', oper4 = 'Z857')
+expect_visit_occurrence(person_id = 1506, visit_start_date = '2020-12-11')
 expect_procedure_occurrence(person_id = 1506, procedure_date = '2020-12-18',
-                            visit_occurrence_id = lookup_visit_occurrence('visit_occurrence_id', person_id = 1006, visit_start_date = '2020-12-18'))
+                            visit_occurrence_id = lookup_visit_occurrence('visit_occurrence_id', person_id = 1506, visit_start_date = '2020-12-11'))
