55c88d0c44664d47a47b44a07e95a187e1e78829
Maxim Moinat <maxim@thehyve.nl>
2020-11-30 22:01:33 +0100

    allow multiple targets for baseline mapping of a field/value

--- src/main/python/field_mapper/FieldConceptMapper.py
+++ src/main/python/field_mapper/FieldConceptMapper.py
@@ -13,9 +13,10 @@
 # GNU General Public License for more details.
 
 # !/usr/bin/env python3
+import copy
 import csv
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 import yaml
 import logging
 
@@ -43,10 +44,6 @@ class FieldConceptMapper:
 
         if in_directory:
             self.load(in_directory)
-
-    def __call__(self, field_id: str, value: str) -> MappingTarget:
-        # Convenience method to allow field_mapper(field_id, value)
-        return self.lookup(field_id, value)
 
     def load(self, directory: Path):
         if not directory.exists():
@@ -144,86 +141,104 @@ class FieldConceptMapper:
             n = len(self.field_mappings)
         return random.sample(list(self.field_mappings.values()), n)
 
-    def lookup(self, field_id: str, value: str) -> Optional[MappingTarget]:
+    def lookup_one(self, field_id: str, value: str) -> Optional[MappingTarget]:
+        pass
+
+    def lookup(self, field_id: str, value: str) -> List[MappingTarget]:
         """
         For given field_id/value pair, looks up the target concept_id, value_as_concept_id, value_as_number and unit_concept_id.
-        The mapping can be one of two types:
-        1. Has a unit. If no mapping for value found, the value is assumed to be numeric (float). Variable maps to concept_id and unit_concept_id.
-        2. Has a value. The field_id/value pair maps to a concept_id and (optionally) a value_as_concept_id.
+
+        The mappings can be one of two types:
+        1. Has a value mapping. The field_id/value pair maps to a concept_id and (optionally) a value_as_concept_id.
+        There can be multiple event and value targets. All combinations are returned. However, typically there will be EITER multiple event targets OR multiple value targets
+        2. No value mapping. If no mapping for value found, the value is assumed to be numeric (float). Variable maps to concept_id and unit_concept_id.
+        If mapping has a unit, values of -1 and -3 are ignored. These are considered 'not known' values.
         :param field_id: string
         :param value: string
-        :return: MappingTarget
+        :return: List[MappingTarget]
         """
-        target = MappingTarget()
-
         field_mapping = self.get_mapping(field_id)
 
         if not field_mapping:
             logger.debug(f'Field "{field_id}" is unknown')
             # Do not map fields not in the mapping tables. These are by default ignored.
-            return None
+            return []
 
         if field_mapping.is_ignored():
-            return None
+            return []
 
         is_discrete = field_mapping.has_values()
 
         if is_discrete:
-            # Create categorical target
+            source_value = field_id + "|" + value
+            # Create categorical target(s)
             value_mapping = field_mapping.values.get(value)
-            target.source_value = field_id + "|" + value
-
             if not value_mapping:
                 logger.warning(f'Value "{value}" for field_id "{field_id}" is unknown')
-                target.concept_id = 0
-                target.source_value = field_id + "|" + value
-                return target
+                return [MappingTarget(0, source_value)]
 
             if value_mapping.is_ignored():
-                return None
+                return []
 
             if not value_mapping.is_approved():
                 logger.debug(f'Value "{value}" for field_id "{field_id}" is not approved')
-                target.concept_id = 0
-                return target
+                return [MappingTarget(0, source_value)]
 
-            if not value_mapping.event_target:
-                target.concept_id = 0
+            if not value_mapping.event_targets:
                 logger.warning(f'"{field_id}-{value}" does not have an event_concept_id associated')
+                target_event_concept_ids = [0]
             else:
-                target.concept_id = value_mapping.event_target.concept_id
+                target_event_concept_ids = value_mapping.get_event_concept_ids()
 
-            if value_mapping.value_target:
-                target.value_as_concept_id = value_mapping.value_target.concept_id
-            return target
+            target_value_as_concept_ids = [None]
+            if value_mapping.value_targets:
+                target_value_as_concept_ids = value_mapping.get_value_concept_ids()
+
+            # Return all combinations of event and value target
+            # Typically, there will be EITER multiple event targets OR multiple value targets
+            result = []
+            for event_concept_id in target_event_concept_ids:
+                for value_as_concept_id in target_value_as_concept_ids:
+                    target = MappingTarget()
+                    target.concept_id = event_concept_id
+                    target.value_as_concept_id = value_as_concept_id
+                    target.source_value = source_value
+                    result.append(target)
+            return result
         else:
-            target.source_value = field_id
+            template_target = MappingTarget()
+            template_target.source_value = field_id
             try:
                 # Create numeric target
-                target.value_as_number = float(value)
+                template_target.value_as_number = float(value)
                 is_numeric = True
             except ValueError:
                 # Create text target
-                target.value_as_string = value
+                template_target.value_as_string = value
                 is_numeric = False
 
             if is_numeric and (value == '-1' or value == '-3'):
                 # Ignore instances of 'Do not know' or 'Prefer not to answer'
-                return None
+                return []
 
             if not field_mapping.is_approved():
                 logger.debug(f'Field_id "{field_id}" is not approved')
-                target.concept_id = 0
-                return target
+                template_target.concept_id = 0
+                return [template_target]
 
-            if not field_mapping.event_target:
-                target.concept_id = 0
+            template_target.unit_concept_id = field_mapping.unit_target.concept_id if field_mapping.unit_target else 0
+
+            if not field_mapping.event_targets:
                 logger.warning(f'"{field_id}" does not have an event_concept_id associated')
-            else:
-                target.concept_id = field_mapping.event_target.concept_id
+                template_target.concept_id = 0
+                return [template_target]
 
-            target.unit_concept_id = field_mapping.unit_target.concept_id if field_mapping.unit_target else 0
-            return target
+            result = []
+            for event_target in field_mapping.event_targets:
+                target = copy.copy(template_target)
+                target.concept_id = event_target.concept_id
+                result.append(target)
+            return result
 
     def lookup_date_field(self, field_id: str) -> str:
         return self.date_field_mapping.get(field_id, self.default_date_field)
@@ -232,21 +247,31 @@ class FieldConceptMapper:
 if __name__ == '__main__':
     mapper = FieldConceptMapper(Path('./resources/baseline_field_mapping'), 'INFO')
 
+    def _print(targets):
+        if len(targets) == 0:
+            print(None)
+        elif len(targets) == 1:
+            print(targets.pop())
+        else:
+            print(f'{len(targets)} targets')
+            for t in targets:
+                print(f'\t{str(t)}')
+
     # Some simple tests
-    print(mapper('50', '180'))  # numeric
-    print(mapper('46', '12.5'))  # numeric
-    print(mapper('30785', '8'))
-    print(mapper('2443', '0'))
-    print(mapper('2443', '1'))
-    print(mapper('4041', '-3'))  # Ignored
-    print(mapper('41256', '0552'))  # unknown field
-    print(mapper('4041', '123'))  # unknown value for categorical field
+    _print(mapper.lookup('50', '180'))  # numeric
+    _print(mapper.lookup('46', '12.5'))  # numeric
+    _print(mapper.lookup('30785', '8'))
+    _print(mapper.lookup('2443', '0'))
+    _print(mapper.lookup('2443', '1'))
+    _print(mapper.lookup('4041', '-3'))  # Ignored
+    _print(mapper.lookup('41256', '0552'))  # unknown field
+    _print(mapper.lookup('4041', '123'))  # unknown value for categorical field
     # field mapping
-    print(mapper('20001', '1064'))  # unknown value for categorical field
+    _print(mapper.lookup('20001', '1064'))  # unknown value for categorical field
 
     # opcs3
-    print(mapper('41256', '118'))
-    print(mapper('41258', '027'))
+    _print(mapper.lookup('41256', '118'))
+    _print(mapper.lookup('41258', '027'))  # one to many
 
     # get the mapping of a field
     # print(mapper.get_mapping('2335'))
@@ -254,7 +279,7 @@ if __name__ == '__main__':
 
     print('Date field lookup')
     print(mapper.lookup_date_field('46'))  # default
-    print(mapper.lookup_date_field('30110')) # custom
+    print(mapper.lookup_date_field('30110'))  # custom
 
     print(f'{len(mapper.field_mappings):>6} field mappings loaded')
     print(f'{sum([len(x.get_values()) for x in mapper.field_mappings.values()]):>6,} value mappings loaded')
