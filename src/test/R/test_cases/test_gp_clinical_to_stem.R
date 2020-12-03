c454d00090667ff67d1a6d6fff845707ba969ef8
Alessia Peviani <alessia@thehyve.nl>
2020-12-03 18:02:30 +0100

    More informative unit test names for gp_clinical units

--- src/test/R/test_cases/test_gp_clinical_to_stem.R
+++ src/test/R/test_cases/test_gp_clinical_to_stem.R
@@ -1,9 +1,7 @@
-
-declareTest(1700, 'GP clinical to stem, read2 blood pressure')
+declareTest(1700, 'GP clinical to stem, readv2 code in read_3 field')
 add_baseline(eid = 1700)
-add_gp_clinical(eid = 1700, event_dt = '01/04/2020', read_2 = '246..')
-expect_measurement(person_id = 1700, measurement_date = '2020-04-01', measurement_type_concept_id = 32817,
-                   measurement_concept_id = 4061103, measurement_source_concept_id = 45471867, measurement_source_value = '246..')
+add_gp_clinical(eid = 1700, event_dt = '19/04/2020', read_2 = '', read_3 = '246A.')
+expect_measurement(person_id = 1700, measurement_concept_id = 4060831, measurement_source_value = '246A.')
 
 declareTest(1701, 'GP clinical to stem, visit lookup')
 add_baseline(eid = 1701)
@@ -34,34 +32,35 @@ add_gp_clinical(eid = 1705, event_dt = '15/04/2020', read_2 = '44J3.', value1 = 
 expect_measurement(person_id = 1705, measurement_concept_id = 37392176, measurement_source_value = '44J3.', measurement_source_concept_id = 45461929,
                    value_as_number = 12)
 
-declareTest(1706, 'GP clinical to stem, DBP with value')
+declareTest(1706, 'GP clinical to stem, unit concept ID (MEA code)')
 add_baseline(eid = 1706)
-add_gp_clinical(eid = 1706, event_dt = '16/04/2020', read_2 = '246A.', value1 = 80.7, data_provider = '2')
-expect_measurement(person_id = 1706, measurement_concept_id = 4060831, measurement_source_value = '246A.', measurement_source_concept_id = 45468573,
-                   value_as_number = 80.7)
+add_gp_clinical(eid = 1706, event_dt = '02/12/2020', read_3 = 'XE2q5', value3 = 'MEA057')
+expect_measurement(person_id = 1706, measurement_date = '2020-12-02', unit_concept_id = 8636, unit_source_value = 'MEA057')
 
-declareTest(1707, 'GP clinical to stem, DBP and SBP with value')
+declareTest(1707, 'GP clinical to stem, unit concept ID (unit symbol)')
 add_baseline(eid = 1707)
-add_gp_clinical(eid = 1707, event_dt = '17/04/2020', read_2 = '246..', value1 = 123, value2 = 85, data_provider = '1')
-expect_measurement(person_id = 1707, measurement_concept_id = 4060831, measurement_source_value = '246..', value_as_number = 85)
-expect_measurement(person_id = 1707, measurement_concept_id = 4062019, measurement_source_value = '246..', value_as_number = 123)
+add_gp_clinical(eid = 1707, event_dt = '02/12/2020', read_2 = '246..', value3 = 'x10^9/l')
+expect_measurement(person_id = 1707, measurement_date = '2020-12-02', unit_concept_id = 9444, unit_source_value = 'x10^9/l')
 
 declareTest(1708, 'GP clinical to stem, readv3 serum urea')
 add_baseline(eid = 1708)
 add_gp_clinical(eid = 1708, event_dt = '18/04/2020', read_2 = '', read_3 = 'XM0lt')
 expect_measurement(person_id = 1708, measurement_concept_id = 37392195, measurement_source_value = 'XM0lt')
 
-declareTest(1709, 'GP clinical to stem, readv2 code in read_3 field')
+declareTest(1709, 'GP clinical to stem, read2 blood pressure')
 add_baseline(eid = 1709)
-add_gp_clinical(eid = 1709, event_dt = '19/04/2020', read_2 = '', read_3 = '246A.')
-expect_measurement(person_id = 1709, measurement_concept_id = 4060831, measurement_source_value = '246A.')
+add_gp_clinical(eid = 1709, event_dt = '01/04/2020', read_2 = '246..')
+expect_measurement(person_id = 1709, measurement_date = '2020-04-01', measurement_type_concept_id = 32817,
+                   measurement_concept_id = 4061103, measurement_source_concept_id = 45471867, measurement_source_value = '246..')
 
-declareTest(1710, 'GP clinical to stem, unit concept ID')
+declareTest(1710, 'GP clinical to stem, DBP with value')
 add_baseline(eid = 1710)
-add_gp_clinical(eid = 1710, event_dt = '02/12/2020', read_3 = 'XE2q5', value3 = 'MEA057')
-expect_measurement(person_id = 1710, measurement_date = '2020-12-02', unit_concept_id = 8636, unit_source_value = 'MEA057')
+add_gp_clinical(eid = 1710, event_dt = '16/04/2020', read_2 = '246A.', value1 = 80.7, data_provider = '2')
+expect_measurement(person_id = 1710, measurement_concept_id = 4060831, measurement_source_value = '246A.', measurement_source_concept_id = 45468573,
+                   value_as_number = 80.7)
 
-declareTest(1711, 'GP clinical to stem, unit concept ID')
+declareTest(1711, 'GP clinical to stem, DBP and SBP with value')
 add_baseline(eid = 1711)
-add_gp_clinical(eid = 1711, event_dt = '02/12/2020', read_2 = '246..', value3 = 'x10^9/l')
-expect_measurement(person_id = 1711, measurement_date = '2020-12-02', unit_concept_id = 9444, unit_source_value = 'x10^9/l')
+add_gp_clinical(eid = 1711, event_dt = '17/04/2020', read_2 = '246..', value1 = 123, value2 = 85, data_provider = '1')
+expect_measurement(person_id = 1711, measurement_concept_id = 4060831, measurement_source_value = '246..', value_as_number = 85)
+expect_measurement(person_id = 1711, measurement_concept_id = 4062019, measurement_source_value = '246..', value_as_number = 123)
