b542fb2b9f6240176fa1cca2381e0dbdc69f4436
Maxim Moinat <maxim@thehyve.nl>
2020-12-04 10:05:01 +0100

    person lookup from wrapper

--- src/main/python/transformation/hesin_to_visit_detail.py
+++ src/main/python/transformation/hesin_to_visit_detail.py
@@ -3,9 +3,7 @@ from __future__ import annotations
 from typing import List, TYPE_CHECKING
 import pandas as pd
 
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-
-from ..core.model import VisitDetail, Person
+from ..core.model import VisitDetail
 from ..util.date_functions import DEFAULT_DATETIME
 
 if TYPE_CHECKING:
@@ -25,58 +23,52 @@ def hesin_to_visit_detail(wrapper: Wrapper) -> List[VisitDetail]:
                                                add_info='ADD_INFO:coding_origin')
 
     records = []
-    with wrapper.db.session_scope() as session:
-        for _, row in source.iterrows():
+    for _, row in source.iterrows():
 
-            query = session.query(Person) \
-                .filter(Person.person_id == row['eid'])
+        person_id = wrapper.lookup_person_id(person_source_value=row['eid'])
+        if not person_id:
+            continue
 
-            try:
-                person_record = query.one()
-                person_id = person_record.person_id
-            except NoResultFound or MultipleResultsFound:
-                continue
+        if pd.isna(row['admidate']):
+            start_date = DEFAULT_DATETIME
+        else:
+            start_date = row['admidate']
 
-            if pd.isna(row['admidate']):
-                start_date = DEFAULT_DATETIME
-            else:
-                start_date = row['admidate']
+        if pd.isna(row['disdate']):
+            end_date = DEFAULT_DATETIME
+        else:
+            end_date = row['disdate']
 
-            if pd.isna(row['disdate']):
-                end_date = DEFAULT_DATETIME
-            else:
-                end_date = row['disdate']
+        data_source = row['dsource']
+        spell_index = str(row['spell_index'])
 
-            data_source = row['dsource']
-            spell_index = str(row['spell_index'])
+        # The dsource contains strings of 3-4 characters and admimeth, admisorc, disdest contrains integers of length 2.
+        # Thus the 50character cut off it is not an issue for losing data, currently.
+        method_source = "record origin:"+data_source+"/admission method:"+str(row['admimeth'])
+        admit_source = "record origin:"+data_source+"/admission source:"+str(row['admisorc'])
+        dis_source = "record origin:"+data_source+"/discharge destination:"+str(row['disdest'])
 
-            # The dsource contains strings of 3-4 characters and admimeth, admisorc, disdest contrains integers of length 2.
-            # Thus the 50character cut off it is not an issue for losing data, currently.
-            method_source = "record origin:"+data_source+"/admission method:"+str(row['admimeth'])
-            admit_source = "record origin:"+data_source+"/admission source:"+str(row['admisorc'])
-            dis_source = "record origin:"+data_source+"/discharge destination:"+str(row['disdest'])
+        visit_occurrence_id = wrapper.lookup_visit_occurrence_id(
+            person_id=person_id,
+            record_source_value=f'HES-{row["spell_index"]}'
+        )
 
-            visit_occurrence_id = wrapper.lookup_visit_occurrence_id(
-                person_id=person_id,
-                record_source_value=f'HES-{row["spell_index"]}'
-            )
-
-            r = VisitDetail(
-                person_id=person_id,
-                visit_detail_concept_id=visit_reason.get((row['admimeth'], row['dsource']), 0),
-                visit_detail_start_date=start_date.date(),
-                visit_detail_start_datetime=start_date,
-                visit_detail_end_date=end_date.date(),
-                visit_detail_end_datetime=end_date,
-                visit_detail_type_concept_id=44818517,  # Visit derived from encounter on claim
-                visit_detail_source_value=method_source,
-                admitting_source_concept_id=admit_reason.get((row['admisorc'], row['dsource']), 0),
-                admitting_source_value=admit_source,
-                discharge_to_concept_id=dis_reason.get((row['disdest'], row['dsource']), 0),
-                discharge_to_source_value=dis_source,
-                visit_occurrence_id=visit_occurrence_id,
-                record_source_value=f'HES-{spell_index}',
-                data_source=f'HES-{data_source}'
-            )
-            records.append(r)
-        return records
+        r = VisitDetail(
+            person_id=person_id,
+            visit_detail_concept_id=visit_reason.get((row['admimeth'], row['dsource']), 0),
+            visit_detail_start_date=start_date.date(),
+            visit_detail_start_datetime=start_date,
+            visit_detail_end_date=end_date.date(),
+            visit_detail_end_datetime=end_date,
+            visit_detail_type_concept_id=44818517,  # Visit derived from encounter on claim
+            visit_detail_source_value=method_source,
+            admitting_source_concept_id=admit_reason.get((row['admisorc'], row['dsource']), 0),
+            admitting_source_value=admit_source,
+            discharge_to_concept_id=dis_reason.get((row['disdest'], row['dsource']), 0),
+            discharge_to_source_value=dis_source,
+            visit_occurrence_id=visit_occurrence_id,
+            record_source_value=f'HES-{spell_index}',
+            data_source=f'HES-{data_source}'
+        )
+        records.append(r)
+    return records
