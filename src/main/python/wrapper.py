66daf5da7d7d58b11f8171f4725f5243956e4084
Maxim Moinat <maxim@thehyve.nl>
2020-12-08 16:42:33 +0100

    add legacy wrapper methods

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -21,6 +21,9 @@ from omop_etl_wrapper.config.models import MainConfig
 from src.main.python.transformation import *
 from src.main.python import cdm
 
+from typing import Optional
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -108,12 +111,75 @@ class Wrapper(BaseWrapper):
     def load_from_stem_table(self):
         # TODO: check whether any values cannot be mapped to corresponding domain (e.g. value_as_string to measurement)
         # Note: the stem_table.id is not used, we use the auto-increment of the domain tables itself.
-        self.execute_sql_file('src/main/sql/stem_table_to_observation.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_measurement.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_condition_occurrence.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_procedure_occurrence.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_drug_exposure.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_device_exposure.sql')
-        self.execute_sql_file('src/main/sql/stem_table_to_specimen.sql')
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_observation.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_measurement.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_condition_occurrence.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_procedure_occurrence.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_drug_exposure.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_device_exposure.sql'))
+        self.execute_sql_file(Path('src/main/sql/stem_table_to_specimen.sql'))
+
+    # TODO: check support for below functions in omop-etl-wrapper
+    def mapping_tables_lookup(self, mapping_file: str, add_info: Optional[str] = None, first_only: bool = True, approved_only: bool = True):
+        """
+        Create a dictionary to lookup target concept_id by source code from a mapping file.
+        If mapping is not APPROVED, it is not included.
+        :param mapping_file: path to the csv file with the mapping
+        :param add_info: for some records we needed to find the standard concept by combine two source fields.
+                         If this parameter is filled the dictionary keys will be a combination of the two fields.
+        :param first_only: if True, return the first available match only (default True). If False, all targets are lists of concept ids.
+        :param approved_only: if True, return only approved mappings (mappingStatus=approved)
+        :return: the dictionary. values are either strings (first_only = True) or lists (first_only = False)
+        """
+        result = {}
+        with open(mapping_file) as f_in:
+            table_mapping = csv.DictReader(f_in, delimiter=',')
+
+            for row in table_mapping:
+                if approved_only and row['mappingStatus'] != 'APPROVED':
+                    continue
+
+                if not add_info:
+                    key = row['sourceCode']
+                else:
+                    key = (row['sourceCode'], row[add_info])
+
+                target = int(row['conceptId'])
+
+                if first_only:
+                    # value is string
+                    if key in result:
+                        # Skip if already exists
+                        logger.warning(f'Source code "{key}" in {mapping_file} occurs twice. Only first is taken.')
+                        continue
+                    else:
+                        result[key] = target
+                else:
+                    # value is list
+                    if key in result:
+                        result[key].append(target)
+                    else:
+                        result[key] = [target]
+        return result
+
+    def lookup_visit_occurrence_id(self, **kwargs) -> Optional[int]:
+        return self.lookup_id(self.cdm.VisitOccurrence, 'visit_occurrence_id', **kwargs)
+
+    def lookup_visit_detail_id(self, **kwargs) -> Optional[int]:
+        return self.lookup_id(self.cdm.VisitDetail, 'visit_detail_id', **kwargs)
+
+    def lookup_person_id(self, person_source_value) -> Optional[int]:
+        return self.lookup_id(self.cdm.Person, 'person_id', person_source_value=person_source_value)
+
+    def lookup_id(self, model, id_to_lookup, **kwargs) -> Optional[int]:
+        query = self._session_for_lookups.query(model).filter_by(**kwargs)
+        try:
+            visit_record = query.one()
+        except NoResultFound:
+            return None
+        except MultipleResultsFound:
+            logger.warning(f'Multiple {id_to_lookup}\'s found for {kwargs}, returning first only')
+            visit_record = query.first()
+        return getattr(visit_record, id_to_lookup)
 
 
--- src/main/sql/stem_table_to_specimen.sql
+++ src/main/sql/stem_table_to_specimen.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO @target_schema.specimen
+INSERT INTO @cdm_schema.specimen
 (
 	person_id,
 	specimen_concept_id,
@@ -48,7 +48,7 @@ SELECT
 
 	stem_table.data_source AS data_source
 
-FROM @target_schema.stem_table
+FROM @cdm_schema.stem_table
     LEFT JOIN vocab.concept USING (concept_id)
 WHERE (stem_table.domain_id = 'Specimen') OR (stem_table.domain_id IS NULL AND (concept.domain_id = 'Specimen' OR concept.domain_id = 'Spec Anatomic Site'))
 ;
