6bd8a0f74e1082a900cc065fdebada52c163a930
Maxim Moinat <maxim@thehyve.nl>
2020-12-08 16:37:30 +0100

    update cdm schema parameterisation in sql

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -38,7 +38,7 @@ class Wrapper(BaseWrapper):
 
         # One session for all lookups done in the wrapper. Note: use for read_only
         # TODO: is still valid?
-        self._session_for_lookups = self.db.get_new_session()
+        # self._session_for_lookups = self.db.get_new_session()
 
     def run(self):
 
@@ -107,14 +107,13 @@ class Wrapper(BaseWrapper):
 
     def load_from_stem_table(self):
         # TODO: check whether any values cannot be mapped to corresponding domain (e.g. value_as_string to measurement)
-        target_schema = 'omopcdm'  # TODO: target_schema from variable
         # Note: the stem_table.id is not used, we use the auto-increment of the domain tables itself.
-        self.execute_sql_file('src/main/sql/stem_table_to_observation.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_measurement.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_condition_occurrence.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_procedure_occurrence.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_drug_exposure.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_device_exposure.sql', target_schema=target_schema)
-        self.execute_sql_file('src/main/sql/stem_table_to_specimen.sql', target_schema=target_schema)
+        self.execute_sql_file('src/main/sql/stem_table_to_observation.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_measurement.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_condition_occurrence.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_procedure_occurrence.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_drug_exposure.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_device_exposure.sql')
+        self.execute_sql_file('src/main/sql/stem_table_to_specimen.sql')
 
 
