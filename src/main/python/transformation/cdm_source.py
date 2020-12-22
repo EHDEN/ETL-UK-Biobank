ee180a15ef722ed0532fc8bf759b3b94342d8933
Maxim Moinat <maxim@thehyve.nl>
2020-12-22 19:59:07 +0100

    additional parameters for cdm_source

--- src/main/python/transformation/cdm_source.py
+++ src/main/python/transformation/cdm_source.py
@@ -1,28 +1,27 @@
 from __future__ import annotations
 from typing import List, TYPE_CHECKING
 from ..core.model.derived_elements import CdmSource
-#from ..core.model.vocabularies import Vocabulary
-from sqlalchemy.orm.session import Session
+from ..core.model.vocabularies import Vocabulary
 from datetime import date
 
 if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
 
-def cdm_source(wrapper: Wrapper, session=Session) -> List[CdmSource]:
-    records = []
-
+def cdm_source(wrapper: Wrapper) -> List[CdmSource]:
     # Get vocabulary info
-    #vocab_info = session.query(Vocabulary).filter_by(vocabulary_id='None').one()
+    with wrapper.db.session_scope() as session:
+        vocab_info = session.query(Vocabulary).filter_by(vocabulary_id='None').one()
+        vocabulary_version = vocab_info.vocabulary_version
 
-    r = CdmSource(
-        cdm_source_name='UK_Biobank',
+    return [CdmSource(
+        cdm_source_name='UK BiobAnk',
+        cdm_source_abbreviation='UKB',
+        cdm_holder='University College London',
+        source_description='UK Biobank is a large-scale biomedical database and research resource, containing in-depth genetic and health information from half a million UK participants. The database is regularly augmented with additional data and is globally accessible to approved researchers undertaking vital research into the most common and life-threatening diseases. It is a major contributor to the advancement of modern medicine and treatment and has enabled several scientific discoveries that improve human health.',
+        source_documentation_reference='https://biobank.ctsu.ox.ac.uk/crystal/index.cgi',
         cdm_etl_reference='https://github.com/EHDEN/ETL-UK-Biobank.git',
         cdm_release_date=date.today(),
         cdm_version='v5.3.1',
-        #vocabulary_version=vocab_info.vocabulary_version
-    )
-
-    records.append(r)
-    return records
-
+        vocabulary_version=vocabulary_version
+    )]
