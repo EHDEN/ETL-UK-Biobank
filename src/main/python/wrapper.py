4f703d53318d70228b2647b2563f229da226e30f
Anne0507 <annevanwinzum@hotmail.com>
2021-01-19 10:15:48 +0100

    Use delphyne's constraint manager fixes #157

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -51,8 +51,12 @@ class Wrapper(BaseWrapper):
         # Load source to concept mappings
         self.vocab_manager.load_stcm()
 
+        self.db.constraint_manager.drop_cdm_constraints()
+
         # Load source data
         self.transform()
+
+        self.db.constraint_manager.add_cdm_constraints()
 
         # Log/write overview of transformations and sources
         self.summarize()
@@ -75,6 +79,8 @@ class Wrapper(BaseWrapper):
         self.execute_transformation(covid_to_visit_occurrence)
         self.execute_transformation(baseline_to_visit_occurrence)
         self.execute_transformation(hesin_to_visit_occurrence)
+
+        self.db.constraint_manager.add_table_constraints('visit_occurrence', add_constraint=False)
 
         self.execute_transformation(hesin_to_visit_detail)
 
