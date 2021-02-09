3ec6c1a6ae5f12464778ae3b0deec80b3c3b1101
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2021-02-09 11:59:58 +0100

    small query explanation

--- resources/baseline_field_mapping/query_no_condition_occ_concept.sql
+++
@@ -1,4 +0,0 @@
-select condition_source_value, count(condition_source_value)
-from omopcdm.condition_occurrence
-where data_source = 'baseline' and condition_concept_id = 0
-group by condition_source_value;
