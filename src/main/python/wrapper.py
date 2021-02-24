ec87d3038cd8e375ead084e8a1be9c066e9dac30
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2021-02-24 15:12:24 +0100

    covid19 records transformation to visit occurrence

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -90,6 +90,8 @@ class Wrapper(BaseWrapper):
         # New sets
         self.execute_transformation(covid19_emis_gp_scripts_to_drug_exposure, bulk=True)
         self.execute_transformation(covid19_tpp_gp_scripts_to_drug_exposure, bulk=True)
+        self.execute_transformation(covid19_emis_gp_clinical_scripts_to_visit_occurrence, bulk=True)
+        self.execute_transformation(covid19_tpp_gp_clinical_scripts_to_visit_occurrence, bulk=True)
 
         # CDM Source
         self.execute_transformation(cdm_source, bulk=True)
