20fa204ce36464d861242a8e3d7500f318b83390
Anne0507 <annevanwinzum@hotmail.com>
2021-02-24 11:31:34 +0100

    use generate code mapper function

--- src/main/python/transformation/covid19_emis_gp_clinical_to_stem_table.py
+++ src/main/python/transformation/covid19_emis_gp_clinical_to_stem_table.py
@@ -13,8 +13,11 @@ def covid19_emis_gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm
     source = wrapper.source_data.get_source_file('covid19_emis_gp_clinical.csv')
     df = source.get_csv_as_df(apply_dtypes=False)
     mapping_lookup = wrapper.mapping_tables_lookup('resources/mapping_tables/gp_clinical_covid.csv')
-    code_to_concept_id = wrapper.generate_code_to_concept_id_dict(df['code'], vocabulary_id='SNOMED')
     unit_lookup = wrapper.mapping_tables_lookup('resources/mapping_tables/covid19_emis_units.csv')
+
+    snomed_mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary(
+            'SNOMED', restrict_to_codes=list(df['code']))
 
     records = []
 
@@ -32,13 +35,14 @@ def covid19_emis_gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm
 
         if row['code'] in ['-99', '-1']:
             continue
-        concept_id = code_to_concept_id.get(row['code'], 0)
+        mapping = snomed_mapper.lookup(row['code'], first_only=True)
 
         # If the concept is not found in the SNOMED vocabulary, check mapping of local codes.
-        if concept_id == 0:
-            concept_id = mapping_lookup.get(row['code'], 0)
+        if mapping.target_concept_id == 0:
+            mapping.target_concept_id = mapping_lookup.get(row['code'], 0)
 
         visit_id = create_covid_visit_occurrence_id(row['eid'], event_date)
+
         if pd.notna(row['unit']) and re.match(r'^(-[\d])', row['unit']):
             unit_concept_id, unit_source_value = None, None
         else:
@@ -52,8 +56,8 @@ def covid19_emis_gp_clinical_to_stem_table(wrapper: Wrapper) -> List[Wrapper.cdm
             start_date=event_date,
             start_datetime=event_date,
             visit_occurrence_id=visit_id,
-            concept_id=concept_id,
-            source_concept_id=0,  # problem with max value for an integer, for example source code 9350510000
+            concept_id=mapping.target_concept_id,
+            source_concept_id=mapping.source_concept_id,
             source_value=row['code'],
             unit_concept_id=unit_concept_id,
             unit_source_value=unit_source_value,
