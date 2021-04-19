afcb7a1d936859931a20463057d8ac12b8b160dd
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2021-04-19 18:05:16 +0200

    add view at target schema

--- src/main/sql/add_vocabulary_views.sql
+++ src/main/sql/add_vocabulary_views.sql
@@ -1,13 +1,13 @@
 -- Keep vocabularies in a separate schema but create a view in OMOP schema
 -- as some OHDSI packages need the data and vocabularies together.
 
-CREATE OR REPLACE VIEW concept AS (SELECT * FROM @vocabulary_schema.concept);
-CREATE OR REPLACE VIEW concept_relationship AS (SELECT * FROM @vocabulary_schema.concept_relationship);
-CREATE OR REPLACE VIEW concept_ancestor AS (SELECT * FROM @vocabulary_schema.concept_ancestor);
-CREATE OR REPLACE VIEW concept_synonym AS (SELECT * FROM @vocabulary_schema.concept_synonym);
-CREATE OR REPLACE VIEW concept_class AS (SELECT * FROM @vocabulary_schema.concept_class);
-CREATE OR REPLACE VIEW domain AS (SELECT * FROM @vocabulary_schema.domain);
-CREATE OR REPLACE VIEW vocabulary AS (SELECT * FROM @vocabulary_schema.vocabulary);
-CREATE OR REPLACE VIEW relationship AS (SELECT * FROM @vocabulary_schema.relationship);
-CREATE OR REPLACE VIEW drug_strength AS (SELECT * FROM @vocabulary_schema.drug_strength);
-CREATE OR REPLACE VIEW source_to_concept_map AS (SELECT * FROM @vocabulary_schema.source_to_concept_map);
+CREATE OR REPLACE VIEW @cdm_schema.concept AS (SELECT * FROM @vocabulary_schema.concept);
+CREATE OR REPLACE VIEW @cdm_schema.concept_relationship AS (SELECT * FROM @vocabulary_schema.concept_relationship);
+CREATE OR REPLACE VIEW @cdm_schema.concept_ancestor AS (SELECT * FROM @vocabulary_schema.concept_ancestor);
+CREATE OR REPLACE VIEW @cdm_schema.concept_synonym AS (SELECT * FROM @vocabulary_schema.concept_synonym);
+CREATE OR REPLACE VIEW @cdm_schema.concept_class AS (SELECT * FROM @vocabulary_schema.concept_class);
+CREATE OR REPLACE VIEW @cdm_schema.domain AS (SELECT * FROM @vocabulary_schema.domain);
+CREATE OR REPLACE VIEW @cdm_schema.vocabulary AS (SELECT * FROM @vocabulary_schema.vocabulary);
+CREATE OR REPLACE VIEW @cdm_schema.relationship AS (SELECT * FROM @vocabulary_schema.relationship);
+CREATE OR REPLACE VIEW @cdm_schema.drug_strength AS (SELECT * FROM @vocabulary_schema.drug_strength);
+CREATE OR REPLACE VIEW @cdm_schema.source_to_concept_map AS (SELECT * FROM @vocabulary_schema.source_to_concept_map);
