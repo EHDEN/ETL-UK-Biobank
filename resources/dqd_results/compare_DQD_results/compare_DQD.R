8e4ea5a2be5cb732b5646c0b0a7d6805fb75436e
Elena <35764654+egarcialara@users.noreply.github.com>
2021-04-29 13:39:41 +0200

    Update compare_dqd

--- resources/dqd_results/compare_DQD_results/compare_DQD.R
+++ resources/dqd_results/compare_DQD_results/compare_DQD.R
@@ -12,7 +12,7 @@ file_old <- "../dqd_results_20210316.json"
 result_old <- jsonlite::fromJSON(file_old)
 
 check_results_old <- result_old$CheckResults %>%
-  select(checkId, CHECK_NAME,
+  select(checkId, CHECK_NAME, 
          CDM_TABLE_NAME, CDM_FIELD_NAME,
          THRESHOLD_VALUE, PCT_VIOLATED_ROWS,
          FAILED)
@@ -22,16 +22,16 @@ file_new <- "../dqd_results_20210425.json"
 result_new <- jsonlite::fromJSON(file_new)
 
 check_results_new <- result_new$CheckResults %>%
-  select(checkId, CHECK_NAME,
+  select(checkId, CHECK_NAME, 
          CDM_TABLE_NAME, CDM_FIELD_NAME,
          THRESHOLD_VALUE, PCT_VIOLATED_ROWS,
-         FAILED)
+         FAILED) %>%
+  rename(new_PCT_VIOLATED_ROWS = PCT_VIOLATED_ROWS)
 
 # ... only keep the different
 combined_results <- tibble(check_results_old) %>%
-  mutate(
-    new_PCT_VIOLATED_ROWS = check_results_new$PCT_VIOLATED_ROWS
-  ) %>%
+  left_join(check_results_new, by=c("CDM_TABLE_NAME", "CDM_FIELD_NAME",
+                                    "CHECK_NAME")) %>%
   filter(PCT_VIOLATED_ROWS != new_PCT_VIOLATED_ROWS)
 
 
@@ -41,24 +41,31 @@ write.csv(combined_results, file="different_checks.csv")
 
 # Plot them - interactive
 p <- combined_results %>%
-  mutate(       # Otherwise legend names are overwritten by ggplotly
-    fails = ifelse(FAILED==0, "Pass", "Fail")
-  ) %>%
+  mutate(       
+    # Otherwise legend names are overwritten by ggplotly
+    fails = ifelse(FAILED.y==0, "Pass", "Fail"),
+    # for aesthetic of labels
+    pct_old = round(PCT_VIOLATED_ROWS*100, digits=2),
+    pct_new = round(new_PCT_VIOLATED_ROWS*100, digits=2)
+  ) %>% 
   ggplot(aes(x=PCT_VIOLATED_ROWS*100, y=new_PCT_VIOLATED_ROWS*100,
-             colour=fails)) +
+             colour=fails,
+             text=paste(
+               sprintf('<br><i>Check ID: </i>%s', checkId.y),
+               sprintf('<br><i>Check name: </i>%s', CHECK_NAME),
+               sprintf('<br><i>Table: </i>%s', CDM_TABLE_NAME),
+               sprintf('<br><i>Field: </i>%s', CDM_FIELD_NAME),
+               sprintf('<br><i>Threshold value: </i>%s', THRESHOLD_VALUE.y),
+               sprintf('<br><b><i>old : </i> %s </b>', pct_old),
+               sprintf('<br><b><i>new : </i> %s </b>', pct_new)
+             ), alpha=0.6)) +
   geom_point() +
   geom_abline(colour="gray", linetype = "dashed")+
-  scale_colour_manual(labels = c("Pass", "Fail"),
-                      values = c("darkolivegreen3", "brown1"))+
+  scale_colour_manual(labels = c("Fail", "Pass"), 
+                      values = c("chocolate1", "darkblue"))+
   labs(x="Previous % of row fails", y="Current % of row fails") +
-  theme_minimal()
-
-ggplotly(p) %>%
-     style(hovertemplate =paste(
-      '<i>Check ID:</i>', combined_results$checkId,
-      '<br><i>Check name:</i>', combined_results$CHECK_NAME,
-      '<br><i>Table:</i>', combined_results$CDM_TABLE_NAME,
-      '<br><i>Field:</i>', combined_results$CDM_FIELD_NAME,
-      '<br><b><i>old %:</i> %{x:.2f}</b>',
-      '<br><b><i>new %:</i> %{y:.2f}</b>')) %>%
+  theme_minimal() 
+  
+ggplotly(p, tooltip="text") %>%
+  style(hoveron="text") %>%
     layout(legend=list(title=list(text=''))) # change legend title here or ggplot
