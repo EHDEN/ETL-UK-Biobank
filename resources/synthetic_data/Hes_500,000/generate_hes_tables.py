c3b633f6b9b4fbd4579b481d0e694080597e10bd
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2021-07-13 14:54:19 +0200

    autogenerate larger synthetic data file

--- resources/synthetic_data/Hes_500,000/generate_hes_tables.py
+++ resources/synthetic_data/Hes_500,000/generate_hes_tables.py
@@ -22,17 +22,18 @@ for i in eid:
 # Create the main hospital data (hesin)
 hesin = pd.DataFrame(hes_temp, columns=['eid', 'ins_index'])
 n_hes = len(hesin)
+print(f'length of main hes: {n_hes}')
 
 # Add columns we use in the transformation with randomised values (hesin)
 hesin['dsource'] = np.random.choice(['HES', 'SMR', 'PEDW'], size=n_hes)
-hesin['epistart'] = min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')
-hesin['epiend'] = min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')
+hesin['epistart'] = (min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')).strftime("%d/%m/%Y")
+hesin['epiend'] = (min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')).strftime("%d/%m/%Y")
 hesin['spell_index'] = np.random.choice(range(1, 967), size=n_hes)
-hesin['elecdate'] = pd.to_datetime('18/02/2010')
-hesin['admidate'] = min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')
+hesin['elecdate'] = '18/02/2010'
+hesin['admidate'] = (min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')).strftime("%d/%m/%Y")
 hesin['admimeth'] = np.random.choice(range(1, 101), size=n_hes)
 hesin['admisorc'] = np.random.choice(range(1, 101), size=n_hes)
-hesin['disdate'] = min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')
+hesin['disdate'] = (min_d + pd.to_timedelta(np.random.randint(d, size=n_hes), unit='d')).strftime("%d/%m/%Y")
 hesin['disdest'] = np.random.choice(range(1, 101), size=n_hes)
 
 # Add to the unused the most frequent value from the scan report (hesin)
@@ -78,15 +79,16 @@ for i in eid:
 # Create the diagnosis data (hesin_diag)
 hesin_diag = pd.DataFrame(diag_temp, columns=['eid', 'ins_index'])
 n_diag = len(hesin_diag)
+print(f'length of hes_diagnosis: {n_diag}')
 
 # Add columns we use in the transformation with randomised values (hesin_diag)
 hesin_diag['level'] = np.random.choice(range(1, 3), size=n_diag)
-hesin_diag['diag_icd9'] = np.random.choice(['V252', '6262', '78909', '4549', '7890',
-                                            '7865', '6359', '4556', '1749', '5509',
-                                            '5742', '6117', '6289'], size=n_diag)
-hesin_diag['diag_icd10'] = np.random.choice(['I10', 'E119', 'Z864', 'E780', 'J459',
-                                             'Z511', 'Z867', 'Z921', 'E039', 'I209',
-                                             'I48', 'I259', 'C509'], size=n_diag)
+hesin_diag['diag_icd9'] = np.random.choice(['V252', '6262', '78909', '4549', '7890', '7865',
+                                            '6359', '4556', '1749', '5509', '5742', '6117',
+                                            '6289'], size=n_diag)
+hesin_diag['diag_icd10'] = np.random.choice(['I10', 'E119', 'Z864', 'E780', 'J459', 'Z511',
+                                             'Z867', 'Z921', 'E039', 'I209', 'I48', 'I259',
+                                             'C509'], size=n_diag)
 
 # Add to the unused the most frequent value from the scan report (hesin_diag)
 hesin_diag['arr_index'] = 0
@@ -104,10 +106,11 @@ for i in eid:
 # Create the diagnosis data (hesin_diag)
 hesin_oper = pd.DataFrame(oper_temp, columns=['eid', 'ins_index'])
 n_oper = len(oper_temp)
+print(f'length of hes_operations: {n_oper}')
 
 # Add columns we use in the transformation with randomised values (hesin_oper)
 hesin_oper['level'] = np.random.choice(range(1, 2), size=n_oper)
-hesin_oper['opdate'] = min_d + pd.to_timedelta(np.random.randint(d, size=n_oper), unit='d')
+hesin_oper['opdate'] = (min_d + pd.to_timedelta(np.random.randint(d, size=n_oper), unit='d')).strftime("%d/%m/%Y")
 hesin_oper['oper3'] = np.random.choice([704, 687, 4011, 387, 6961, 6512, 3811,
                                         608, 7421, 913, 979], size=n_oper)
 hesin_oper['oper4'] = np.random.choice(['X998', 'Z942', 'Z943', 'G451', 'X403', 'Y981', 'H229',
