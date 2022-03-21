96b73aaa90a45d5bf75ddc7ae5c0b3e1bf00a68c
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2022-03-21 10:29:14 +0100

    secondary cause of death transformation

---
+++ src/main/python/transformation/secondary_death_to_observation.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import List, TYPE_CHECKING
+import pandas as pd
+
+from ..util import refactor_icdx_code
+
+if TYPE_CHECKING:
+    from src.main.python.wrapper import Wrapper
+
+
+def secondary_death_to_observation(wrapper: Wrapper) -> List[Wrapper.cdm.Death]:
+    source = wrapper.source_data.get_source_file('baseline.csv')
+    df = source.get_csv_as_df(apply_dtypes=False, usecols=['eid', '40000-0.0', '40002-0.0'])
+
+    df['ICD10_dot'] = df['40002-0.0'].apply(refactor_icdx_code)
+
+    mapper = \
+        wrapper.code_mapper.generate_code_mapping_dictionary(
+            'ICD10', restrict_to_codes=list(df['ICD10_dot']))
+    persons = []
+
+    with wrapper.db.session_scope() as session:
+        # Get the existing death records and check if they are in baseline, if so they are added to the persons list.
+        query_1 = session.query(wrapper.cdm.Death) \
+            .filter(wrapper.cdm.Death.person_id.in_(df['eid'].astype(int).tolist()))
+        for q in query_1:
+            persons.append(q.person_id)
+
+        for _, row in df.iterrows():
+            if pd.isna(row['40000-0.0']):
+                continue
+            if pd.isna(row['40002-0.0']):
+                continue
+            # If the person is not in the existing death records, proceed.
+            if int(row['eid']) not in persons:
+                target = mapper.lookup(row['ICD10_dot'], first_only=True)
+                yield wrapper.cdm.Death(
+                    person_id=int(row['eid']),
+                    observation_concept_id=target.target_concept_id,
+                    observation_date=row['40000-0.0'],
+                    observation_datetime=row['40000-0.0'],
+                    observation_type_concept_id=32815,  # Death Certificate
+                    observation_source_concept_id=target.source_concept_id,
+                    observation_source_value=row['40002-0.0']
+                )
